cb84d0fd198183938bd2080dd54efbdc001fc42d
Oliver Rietmann <rioliver@student.ethz.ch>
2019-11-05 18:39:43 +0100

    Deleted unecessary cmake files and lines

--- cmake/functions.cmake
+++
@@ -1,4 +0,0 @@
-# This File only references other files which contains CMake functions that can
-# be called from other CMakeLists.txt files.
-
-include("${CMAKE_CURRENT_LIST_DIR}/HunterGate.cmake")
