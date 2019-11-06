87398f5473498f44ac2be98bc57c1728d6ac2aca
Erick <erick.schulz@sam.math.ethz.ch>
2019-11-06 14:42:21 +0100

    IncidenceMatrices fixed

--- homeworks/IncidenceMatrices/mastersolution/incidence_mat.cc
+++ homeworks/IncidenceMatrices/mastersolution/incidence_mat.cc
@@ -2,41 +2,47 @@
 
 using namespace Eigen;
 using lf::mesh::Mesh;
+using size_type = lf::base::size_type;
 
-namespace IncidenceMatrices {
+namespace IncidenceMatrices
+{
 
 // @brief Create the mesh consisting of a triangle and quadrilateral
 //        from the exercise sheet.
 // @return Shared pointer to the hybrid2d mesh.
-std::shared_ptr<Mesh> createDemoMesh() {
-  // data type for a hybrid mesh in a world of dimension 2
-  lf::mesh::hybrid2d::MeshFactory builder(2);
+std::shared_ptr<Mesh> createDemoMesh()
+{
+  // builder for a hybrid mesh in a world of dimension 2
+  std::shared_ptr<lf::mesh::hybrid2d::MeshFactory> mesh_factory_ptr =
+      std::make_shared<lf::mesh::hybrid2d::MeshFactory>(2);
 
   // Add points
-  builder.AddPoint(Vector2d{0, 0});    // (0)
-  builder.AddPoint(Vector2d{1, 0});    // (1)
-  builder.AddPoint(Vector2d{1, 1});    // (2)
-  builder.AddPoint(Vector2d{0, 1});    // (3)
-  builder.AddPoint(Vector2d{0.5, 1});  // (4)
+  mesh_factory_ptr->AddPoint(Vector2d{0, 0});   // (0)
+  mesh_factory_ptr->AddPoint(Vector2d{1, 0});   // (1)
+  mesh_factory_ptr->AddPoint(Vector2d{1, 1});   // (2)
+  mesh_factory_ptr->AddPoint(Vector2d{0, 1});   // (3)
+  mesh_factory_ptr->AddPoint(Vector2d{0.5, 1}); // (4)
 
   // Add the triangle
   // First set the coordinates of its nodes:
   MatrixXd nodesOfTria(2, 3);
   nodesOfTria << 1, 1, 0.5, 0, 1, 1;
-  builder.AddEntity(
-      lf::base::RefEl::kTria(),  // we want a triangle
-      {1, 2, 4},                 // indices of the nodes
-      std::make_unique<lf::geometry::TriaO1>(nodesOfTria));  // node coords
+  mesh_factory_ptr->AddEntity(
+      lf::base::RefEl::kTria(),                        // we want a triangle
+      nonstd::span<const size_type>({1, 2, 4}),        // indices of the nodes
+      std::unique_ptr<lf::geometry::TriaO1>(nullptr)); // node coords
 
   // Add the quadrilateral
   MatrixXd nodesOfQuad(2, 4);
   nodesOfQuad << 0, 1, 0.5, 0, 0, 0, 1, 1;
-  builder.AddEntity(lf::base::RefEl::kQuad(), {0, 1, 4, 3},
-                    std::make_unique<lf::geometry::QuadO1>(nodesOfQuad));
+  mesh_factory_ptr->AddEntity(
+      lf::base::RefEl::kQuad(),
+      nonstd::span<const size_type>({0, 1, 4, 3}),
+      std::make_unique<lf::geometry::QuadO1>(nodesOfQuad));
 
-  std::shared_ptr<Mesh> demoMesh = builder.Build();
+  std::shared_ptr<Mesh> demoMesh_p = mesh_factory_ptr->Build();
 
-  return demoMesh;
+  return demoMesh_p;
 }
 
 // @brief Compute the edge-vertex incidence matrix G for a given mesh
@@ -44,7 +50,8 @@ std::shared_ptr<Mesh> createDemoMesh() {
 //             such as lf::mesh::hybrid2d::Mesh)
 // @return The edge-vertex incidence matrix as Eigen::SparseMatrix<int>
 /* SAM_LISTING_BEGIN_1 */
-SparseMatrix<int> computeEdgeVertexIncidenceMatrix(const Mesh& mesh) {
+SparseMatrix<int> computeEdgeVertexIncidenceMatrix(const Mesh &mesh)
+{
   // Store edge-vertex incidence matrix here
   SparseMatrix<int, RowMajor> G;
 
@@ -64,15 +71,16 @@ SparseMatrix<int> computeEdgeVertexIncidenceMatrix(const Mesh& mesh) {
   // introduced as "distribute scheme" in class. We cannot iterative over
   // vertices, because LehrFEM++ does not allow to visit the edges
   // adjacent to a vertex
-  for (const auto& edge : mesh.Entities(1)) {
+  for (const lf::mesh::Entity *edge : mesh.Entities(1))
+  {
     // Get index of this edge
-    std::size_t edgeIdx = mesh.Index(edge);
+    size_type edgeIdx = mesh.Index(*edge);
     // Get the nodes and their indices.
     // Note, that seen from the edges the nodes have codim 1, not 2,
     // hence we call SubEntities(1). This is a relative codimension!
-    auto nodes = edge.SubEntities(1);
-    std::size_t firstNodeIdx = mesh.Index(nodes[0]),
-                lastNodeIdx = mesh.Index(nodes[1]);
+    auto nodes = edge->SubEntities(1);
+    size_type firstNodeIdx = mesh.Index(*nodes[0]);
+    size_type lastNodeIdx = mesh.Index(*nodes[1]);
     // Add the matrix entries according to the definition
     G.coeffRef(edgeIdx, firstNodeIdx) += 1;
     G.coeffRef(edgeIdx, lastNodeIdx) -= 1;
@@ -87,7 +95,8 @@ SparseMatrix<int> computeEdgeVertexIncidenceMatrix(const Mesh& mesh) {
 //             such as lf::mesh::hybrid2d::Mesh)
 // @return The cell-edge incidence matrix as Eigen::SparseMatrix<int>
 /* SAM_LISTING_BEGIN_2 */
-SparseMatrix<int> computeCellEdgeIncidenceMatrix(const Mesh& mesh) {
+SparseMatrix<int> computeCellEdgeIncidenceMatrix(const Mesh &mesh)
+{
   // Store cell-edge incidence matrix here
   SparseMatrix<int, RowMajor> D;
 
@@ -105,21 +114,23 @@ SparseMatrix<int> computeCellEdgeIncidenceMatrix(const Mesh& mesh) {
   // To compute D efficiently we iterate over all cells and check the
   // orientations (+1 or -1, same as in the definition of the matrix D)
   // of its edges. For this we may use RelativeOrientations().
-  for (const auto& cell : mesh.Entities(0)) {
+  for (const lf::mesh::Entity *cell : mesh.Entities(0))
+  {
     // Get cell index
-    std::size_t cellIdx = mesh.Index(cell);
+    size_type cellIdx = mesh.Index(*cell);
     // Get edges and their orientations (these already the entries for D!)
-    auto edges = cell.SubEntities(1);
-    auto edgeOrientations = cell.RelativeOrientations();
+    auto edges = cell->SubEntities(1);
+    auto edgeOrientations = cell->RelativeOrientations();
 
     // Iterate over both and add to D
     auto edgeIt = edges.begin();
     auto orntIt = edgeOrientations.begin();
     for (; edgeIt != edges.end() && orntIt != edgeOrientations.end();
-         ++edgeIt, ++orntIt) {
+         ++edgeIt, ++orntIt)
+    {
       // Get the edge index and add its orientation to D
-      std::size_t edgeIdx = mesh.Index(*edgeIt);
-      D.coeffRef(cellIdx, edgeIdx) += to_sign(*orntIt);
+      size_type edgeIdx = mesh.Index(**edgeIt);
+      D.coeffRef(cellIdx, edgeIdx) += lf::mesh::to_sign(*orntIt);
     }
   }
   /* END_SOLUTION */
@@ -134,7 +145,8 @@ SparseMatrix<int> computeCellEdgeIncidenceMatrix(const Mesh& mesh) {
 //             such as lf::mesh::hybrid2d::Mesh)
 // @return true, if the product is zero and false otherwise
 /* SAM_LISTING_BEGIN_3 */
-bool testZeroIncidenceMatrixProduct(const Mesh& mesh) {
+bool testZeroIncidenceMatrixProduct(const Mesh &mesh)
+{
   bool isZero = false;
 
   /* BEGIN_SOLUTION */
@@ -153,4 +165,4 @@ bool testZeroIncidenceMatrixProduct(const Mesh& mesh) {
 }
 /* SAM_LISTING_END_3 */
 
-}  // namespace IncidenceMatrices
+} // namespace IncidenceMatrices
--- homeworks/IncidenceMatrices/mysolution/incidence_mat.cc
+++ homeworks/IncidenceMatrices/mysolution/incidence_mat.cc
@@ -2,48 +2,55 @@
 
 using namespace Eigen;
 using lf::mesh::Mesh;
+using size_type = lf::base::size_type;
 
-namespace IncidenceMatrices {
+namespace IncidenceMatrices
+{
 
 // @brief Create the mesh consisting of a triangle and quadrilateral
 //        from the exercise sheet.
 // @return Shared pointer to the hybrid2d mesh.
-std::shared_ptr<Mesh> createDemoMesh() {
-  // data type for a hybrid mesh in a world of dimension 2
-  lf::mesh::hybrid2d::MeshFactory builder(2);
+std::shared_ptr<Mesh> createDemoMesh()
+{
+  // builder for a hybrid mesh in a world of dimension 2
+  std::shared_ptr<lf::mesh::hybrid2d::MeshFactory> mesh_factory_ptr =
+      std::make_shared<lf::mesh::hybrid2d::MeshFactory>(2);
 
   // Add points
-  builder.AddPoint(Vector2d{0, 0});    // (0)
-  builder.AddPoint(Vector2d{1, 0});    // (1)
-  builder.AddPoint(Vector2d{1, 1});    // (2)
-  builder.AddPoint(Vector2d{0, 1});    // (3)
-  builder.AddPoint(Vector2d{0.5, 1});  // (4)
+  mesh_factory_ptr->AddPoint(Vector2d{0, 0});   // (0)
+  mesh_factory_ptr->AddPoint(Vector2d{1, 0});   // (1)
+  mesh_factory_ptr->AddPoint(Vector2d{1, 1});   // (2)
+  mesh_factory_ptr->AddPoint(Vector2d{0, 1});   // (3)
+  mesh_factory_ptr->AddPoint(Vector2d{0.5, 1}); // (4)
 
   // Add the triangle
   // First set the coordinates of its nodes:
   MatrixXd nodesOfTria(2, 3);
   nodesOfTria << 1, 1, 0.5, 0, 1, 1;
-  builder.AddEntity(
-      lf::base::RefEl::kTria(),  // we want a triangle
-      {1, 2, 4},                 // indices of the nodes
-      std::make_unique<lf::geometry::TriaO1>(nodesOfTria));  // node coords
+  mesh_factory_ptr->AddEntity(
+      lf::base::RefEl::kTria(),                          // we want a triangle
+      nonstd::span<const size_type>({1, 2, 4}),          // indices of the nodes
+      std::unique_ptr<lf::geometry::Geometry>(nullptr)); // node coords
 
   // Add the quadrilateral
   MatrixXd nodesOfQuad(2, 4);
   nodesOfQuad << 0, 1, 0.5, 0, 0, 0, 1, 1;
-  builder.AddEntity(lf::base::RefEl::kQuad(), {0, 1, 4, 3},
-                    std::make_unique<lf::geometry::QuadO1>(nodesOfQuad));
+  mesh_factory_ptr->AddEntity(
+      lf::base::RefEl::kQuad(),
+      nonstd::span<const size_type>({0, 1, 4, 3}),
+      std::unique_ptr<lf::geometry::Geometry>(nullptr));
 
-  std::shared_ptr<Mesh> demoMesh = builder.Build();
+  std::shared_ptr<Mesh> demoMesh_p = mesh_factory_ptr->Build();
 
-  return demoMesh;
+  return demoMesh_p;
 }
 
 // @brief Compute the edge-vertex incidence matrix G for a given mesh
 // @param mesh The input mesh of type lf::mesh::Mesh (or of derived type,
 //             such as lf::mesh::hybrid2d::Mesh)
 // @return The edge-vertex incidence matrix as Eigen::SparseMatrix<int>
-SparseMatrix<int> computeEdgeVertexIncidenceMatrix(const Mesh& mesh) {
+SparseMatrix<int> computeEdgeVertexIncidenceMatrix(const Mesh &mesh)
+{
   // Store edge-vertex incidence matrix here
   SparseMatrix<int, RowMajor> G;
 
@@ -58,7 +65,8 @@ SparseMatrix<int> computeEdgeVertexIncidenceMatrix(const Mesh& mesh) {
 // @param mesh The input mesh of type lf::mesh::Mesh (or of derived type,
 //             such as lf::mesh::hybrid2d::Mesh)
 // @return The cell-edge incidence matrix as Eigen::SparseMatrix<int>
-SparseMatrix<int> computeCellEdgeIncidenceMatrix(const Mesh& mesh) {
+SparseMatrix<int> computeCellEdgeIncidenceMatrix(const Mesh &mesh)
+{
   // Store cell-edge incidence matrix here
   SparseMatrix<int, RowMajor> D;
 
@@ -74,7 +82,8 @@ SparseMatrix<int> computeCellEdgeIncidenceMatrix(const Mesh& mesh) {
 // @param mesh The input mesh of type lf::mesh::Mesh (or of derived type,
 //             such as lf::mesh::hybrid2d::Mesh)
 // @return true, if the product is zero and false otherwise
-bool testZeroIncidenceMatrixProduct(const Mesh& mesh) {
+bool testZeroIncidenceMatrixProduct(const Mesh &mesh)
+{
   bool isZero = false;
 
   /* BEGIN_SOLUTION */
@@ -84,4 +93,4 @@ bool testZeroIncidenceMatrixProduct(const Mesh& mesh) {
   return isZero;
 }
 
-}  // namespace IncidenceMatrices
+} // namespace IncidenceMatrices
