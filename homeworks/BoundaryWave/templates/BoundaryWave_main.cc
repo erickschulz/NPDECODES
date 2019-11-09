c9fd3a045980e9bdbf27d4471e933c8b9ab748c4
Oliver Rietmann <rioliver@student.ethz.ch>
2019-11-09 17:09:12 +0100

    Removed boost from BoundaryWave

--- homeworks/BoundaryWave/templates/BoundaryWave_main.cc
+++ homeworks/BoundaryWave/templates/BoundaryWave_main.cc
@@ -20,11 +20,8 @@ int main(int /*argc*/, const char ** /*argv*/) {
 
   /* TOOLS AND DATA */
   // Load mesh into a Lehrfem++ object
-  boost::filesystem::path here = __FILE__;
-  std::string filename = "/meshes/BoundaryWave.msh";
-  auto mesh_path = here.parent_path().parent_path() / filename;
   auto mesh_factory = std::make_unique<lf::mesh::hybrid2d::MeshFactory>(2);
-  const lf::io::GmshReader reader(std::move(mesh_factory), mesh_path.string());
+  const lf::io::GmshReader reader(std::move(mesh_factory), CURRENT_SOURCE_DIR"/meshes/BoundaryWave.msh");
   auto mesh_p = reader.mesh();  // type shared_ptr< const lf::mesh::Mesh>
   // Finite element space
   auto fe_space_p =
