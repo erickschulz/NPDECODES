212640f204b79b3ce530c2218118a6542dc6c953
Oliver Rietmann <rioliver@student.ethz.ch>
2019-11-09 17:13:07 +0100

    Removed boost from NonConformingCrouzeixRaviartFiniteElements

--- homeworks/NonConformingCrouzeixRaviartFiniteElements/mastersolution/non_conforming_crouzeix_raviart_finite_elements_main.cc
+++ homeworks/NonConformingCrouzeixRaviartFiniteElements/mastersolution/non_conforming_crouzeix_raviart_finite_elements_main.cc
@@ -8,18 +8,12 @@
 
 #include <lf/assemble/assemble.h>
 #include <lf/io/io.h>
-#include <boost/filesystem.hpp>
 #include <iomanip>
 #include "l2_error_cr_discretization_dirichlet_bvp.h"
 
 using namespace NonConformingCrouzeixRaviartFiniteElements;
 
-int main()
-{
-  // Obtain paths to mesh files
-  boost::filesystem::path file_path = __FILE__;
-  auto mesh_dir_path = file_path.parent_path().parent_path() / "meshes";
-
+int main() {
   auto sep = std::setw(15);
   std::cout << std::left << sep << "Number of dofs"
             << " | "
@@ -28,13 +22,11 @@ int main()
   // Loop over meshes and output number of dofs and L2 norm of error
   for (int i = 1; i <= 4; ++i)
   {
-    std::string mesh_path =
-        (mesh_dir_path / ("refined_square" + std::to_string(i) + ".msh"))
-            .string();
+    std::string mesh_file = CURRENT_SOURCE_DIR"/meshes/refined_square" + std::to_string(i) + ".msh";
 
     // Read mesh from file
     auto mesh_factory = std::make_unique<lf::mesh::hybrid2d::MeshFactory>(2);
-    const lf::io::GmshReader reader(std::move(mesh_factory), mesh_path);
+    const lf::io::GmshReader reader(std::move(mesh_factory), mesh_file);
     auto mesh_ptr = reader.mesh();
 
     // TODO: 2-14.i)
@@ -50,7 +42,7 @@ int main()
     // TODO: 2-14.y)
     /* BEGIN_SOLUTION */
     std::cout << std::left << sep << dof_handler.NumDofs() << " | "
-              << L2errorCRDiscretizationDirichletBVP(mesh_path) << std::endl;
+              << L2errorCRDiscretizationDirichletBVP(mesh_file) << std::endl;
     /* END_SOLUTION */
   }
 
--- homeworks/NonConformingCrouzeixRaviartFiniteElements/template/non_conforming_crouzeix_raviart_finite_elements_main.cc
+++ homeworks/NonConformingCrouzeixRaviartFiniteElements/template/non_conforming_crouzeix_raviart_finite_elements_main.cc
@@ -8,17 +8,12 @@
 
 #include <lf/assemble/assemble.h>
 #include <lf/io/io.h>
-#include <boost/filesystem.hpp>
 #include <iomanip>
 #include "l2_error_cr_discretization_dirichlet_bvp.h"
 
 using namespace NonConformingCrouzeixRaviartFiniteElements;
 
 int main() {
-  // Obtain paths to mesh files
-  boost::filesystem::path file_path = __FILE__;
-  auto mesh_dir_path = file_path.parent_path().parent_path() / "meshes";
-
   auto sep = std::setw(15);
   std::cout << std::left << sep << "Number of dofs"
             << " | "
@@ -26,13 +21,11 @@ int main() {
 
   // Loop over meshes and output number of dofs and L2 norm of error
   for (int i = 1; i <= 4; ++i) {
-    std::string mesh_path =
-        (mesh_dir_path / ("refined_square" + std::to_string(i) + ".msh"))
-            .string();
+    std::string mesh_file = CURRENT_SOURCE_DIR"/meshes/refined_square" + std::to_string(i) + ".msh";
 
     // Read mesh from file
     auto mesh_factory = std::make_unique<lf::mesh::hybrid2d::MeshFactory>(2);
-    const lf::io::GmshReader reader(std::move(mesh_factory), mesh_path);
+    const lf::io::GmshReader reader(std::move(mesh_factory), mesh_file);
     auto mesh_ptr = reader.mesh();
 
     // TODO: 2-14.i)
