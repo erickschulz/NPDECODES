35a0171cac9c384c3cefab7d6f323d8c229cc91a
Oliver Rietmann <rioliver@student.ethz.ch>
2019-11-18 21:41:58 +0100

    Added HandlingDOFs/ to homeworks/

---
+++ homeworks/HandlingDOFs/mastersolution/dependencies.cmake
@@ -0,0 +1,19 @@
+# Dependencies of mastersolution:
+
+# DIR will be provided by the calling file.
+
+set(SOURCES
+  ${DIR}/handling_dofs_main.cc
+  ${DIR}/handling_dofs.h
+  ${DIR}/handling_dofs.cc
+)
+
+set(LIBRARIES
+  Eigen3::Eigen
+  LF::lf.assemble
+  LF::lf.base
+  LF::lf.geometry
+  LF::lf.mesh
+  LF::lf.mesh.utils
+  LF::lf.mesh.test_utils
+)
