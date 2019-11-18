61875652e185b521c5da98b7ec9f592e4c3b463d
Oliver Rietmann <rioliver@student.ethz.ch>
2019-11-18 21:32:25 +0100

    Added ErrorEstimatesForTraces/ to developers/

---
+++ developers/ErrorEstimatesForTraces/mastersolution/dependencies.cmake
@@ -0,0 +1,25 @@
+#if SOLUTION
+# Dependencies of mastersolution:
+#else
+# Add your custom dependencies here:
+#endif
+
+# DIR will be provided by the calling file.
+
+set(SOURCES
+  ${DIR}/trace_error_estimate_main.cc
+  ${DIR}/tee_lapl_robin_assembly.h
+  ${DIR}/tee_lapl_robin_assembly.cc
+)
+
+set(LIBRARIES
+  Eigen3::Eigen
+  LF::lf.assemble
+  LF::lf.geometry
+  LF::lf.io
+  LF::lf.mesh.hybrid2d
+  LF::lf.mesh.test_utils
+  LF::lf.mesh.utils
+  LF::lf.refinement
+  LF::lf.uscalfe
+)
