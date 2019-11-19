1dbe9b6aac6eafdbdf99b4e2d632ff4d6a38de06
Oliver Rietmann <rioliver@student.ethz.ch>
2019-11-19 19:49:11 +0100

    Added developers/TransformationOfGalerkinMatrices/

---
+++ developers/TransformationOfGalerkinMatrices/mastersolution/dependencies.cmake
@@ -0,0 +1,17 @@
+#if SOLUTION
+# Dependencies of mastersolution:
+#else
+# Add your custom dependencies here:
+#endif
+
+# DIR will be provided by the calling file.
+
+set(SOURCES
+  ${DIR}/TransformationOfGalerkinMatrices.cc
+  ${DIR}/trans_gal_mat.h
+  ${DIR}/trans_gal_mat.cc
+)
+
+set(LIBRARIES
+  Eigen3::Eigen
+)
