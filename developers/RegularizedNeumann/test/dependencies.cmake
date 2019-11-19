4e4d1398cedd9a62ff999c5ece106502e858aad8
Oliver Rietmann <rioliver@student.ethz.ch>
2019-11-19 16:35:26 +0100

    Added developers/

---
+++ developers/RegularizedNeumann/test/dependencies.cmake
@@ -0,0 +1,19 @@
+#if SOLUTION
+# Dependencies of mastersolution tests:
+#else
+# Add your custom dependencies here:
+#endif
+
+# PROBLEM_NAME and DIR will be provided by the calling file.
+
+set(SOURCES
+  test/regularizedneumann_test_${DIR}.cc
+)
+
+set(LIBRARIES
+  Eigen3::Eigen
+  GTest::gtest_main
+  LF::lf.io
+  LF::lf.mesh.hybrid2d
+  LF::lf.uscalfe
+)
