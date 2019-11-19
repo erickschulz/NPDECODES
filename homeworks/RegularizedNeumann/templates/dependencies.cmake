eb9a7585cac13ca6ecb582d8e2e7637afc5bc56e
Oliver Rietmann <rioliver@student.ethz.ch>
2019-11-19 16:53:35 +0100

    Added homeworks/RegularizedNeumann/

---
+++ homeworks/RegularizedNeumann/templates/dependencies.cmake
@@ -0,0 +1,20 @@
+# Add your custom dependencies here:
+
+# DIR will be provided by the calling file.
+
+set(SOURCES
+  ${DIR}/RegularizedNeumann_main.cc
+  ${DIR}/get_galerkin_lse.h
+  ${DIR}/regNeumann.h
+)
+
+set(LIBRARIES
+  Eigen3::Eigen
+  LF::lf.assemble
+  LF::lf.base
+  LF::lf.geometry
+  LF::lf.mesh.hybrid2d
+  LF::lf.mesh.test_utils
+  LF::lf.mesh.utils
+  LF::lf.uscalfe
+)
