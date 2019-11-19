27deb6f24f14479cc1d24655d9934ec10cc2e251
Oliver Rietmann <rioliver@student.ethz.ch>
2019-11-19 16:34:59 +0100

    Added developers/OutputImpedanceBVP/

---
+++ developers/OutputImpedanceBVP/mastersolution/evalclass.h
@@ -0,0 +1,43 @@
+#ifndef EVALCLASS_HPP
+#define EVALCLASS_HPP
+
+/** @file
+ * @brief NPDE OutputImpedanceBVP
+ * @author Erick Schulz
+ * @date 12/07/2019
+ * @copyright Developed at ETH Zurich
+ */
+
+#include "OutputImpedanceBVP.h"
+
+#include <iostream>
+
+#include <Eigen/Core>
+
+namespace OutputImpedanceBVP {
+
+/* SAM_LISTING_BEGIN_1 */
+class EvalResponse {
+public:
+  /* Constructor */
+  explicit EvalResponse(
+      const std::shared_ptr<lf::uscalfe::FeSpaceLagrangeO1<double>>
+          &fe_space_p);
+  /* Evaluation operator */
+  double operator()(Eigen::Vector2d g, Eigen::Vector2d d) const;
+
+private:
+#if SOLUTION
+  // Matrix representation of bilinear form realized by the evaluation operator.
+  Eigen::Matrix<double, 2, 2> F_Mat_;
+#else
+  //====================
+  // Your code goes here
+  //====================
+#endif
+}; // class EvalResponse
+/* SAM_LISTING_END_1 */
+
+} // namespace OutputImpedanceBVP
+
+#endif
