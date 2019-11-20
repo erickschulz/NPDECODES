3e932af4d6911f3186e8cdda1c227f3c1372e7f5
Oliver Rietmann <rioliver@student.ethz.ch>
2019-11-20 16:39:57 +0100

    Script can copy even if no tests are present

--- scripts/make_templates.sh
+++ scripts/make_templates.sh
@@ -4,21 +4,28 @@ script_dir=$(dirname $0)
 root=$script_dir/..
 
 for dir in $@; do
-
   ProblemName=$(basename $dir)
   echo "-- "$ProblemName
 
   input_dir=$root/developers/$ProblemName
   output_dir=$root/homeworks/$ProblemName
 
+  # plain copy
   cp -r $input_dir/. $output_dir
+
+  # problems
   cp -r $output_dir/mastersolution/. $output_dir/mysolution
-  rename _mastersolution.cc _mysolution.cc $output_dir/test/*_mastersolution.cc
-  cp $input_dir/test/*_mastersolution.cc $output_dir/test/
-
-  unifdef -DSOLUTION=1 -x 2 -m $output_dir/mastersolution/* $output_dir/test/*_mastersolution.cc
-  unifdef -DSOLUTION=0 -x 2 -m $output_dir/mysolution/* $output_dir/test/*_mysolution.cc
-
+  unifdef -DSOLUTION=1 -x 2 -m $output_dir/mastersolution/*
+  unifdef -DSOLUTION=0 -x 2 -m $output_dir/mysolution/*
   cp -r $output_dir/mysolution/. $output_dir/templates
 
+  if [ -d "$output_dir/test" ]; then
+    # tests
+    rename _mastersolution.cc _mysolution.cc $output_dir/test/*_mastersolution.cc
+    cp $input_dir/test/*_mastersolution.cc $output_dir/test/
+    unifdef -DSOLUTION=1 -x 2 -m $output_dir/test/*_mastersolution.cc
+    unifdef -DSOLUTION=0 -x 2 -m $output_dir/test/*_mysolution.cc
+  else
+    echo "     Warning: Found no unit tests for $ProblemName"
+  fi
 done
