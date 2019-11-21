c184213f6bbc37d536809f8240187dfccc8f2827
Oliver Rietmann <rioliver@student.ethz.ch>
2019-11-21 15:55:25 +0100

    Added developers/AvgValBoundary/

---
+++ developers/AvgValBoundary/mastersolution/dependencies.cmake
@@ -0,0 +1,24 @@
+#if SOLUTION
+# Dependencies of mastersolution:
+#else
+# Add your custom dependencies here:
+#endif
+
+# DIR will be provided by the calling file.
+
+set(SOURCES
+  ${DIR}/main.cc
+  ${DIR}/avg_val_boundary.cc
+  ${DIR}/comp_gal_mat.h
+)
+
+set(LIBRARIES
+  Eigen3::Eigen
+  LF::lf.assemble
+  LF::lf.base
+  LF::lf.io
+  LF::lf.mesh.test_utils
+  LF::lf.mesh.utils
+  LF::lf.refinement
+  LF::lf.uscalfe
+)
