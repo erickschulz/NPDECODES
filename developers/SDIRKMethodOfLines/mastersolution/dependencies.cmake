febfc5c05a6d88c11bb966ad7b144c4170a30e13
Oliver Rietmann <rioliver@student.ethz.ch>
2019-11-21 21:04:43 +0100

    Added developers/SDIRKMethodOfLines/

---
+++ developers/SDIRKMethodOfLines/mastersolution/dependencies.cmake
@@ -0,0 +1,27 @@
+#if SOLUTION
+# Dependencies of mastersolution:
+#else
+# Add your custom dependencies here:
+#endif
+
+# DIR will be provided by the calling file.
+
+set(SOURCES
+  ${DIR}/sdirkmethodoflines_main.cc
+  ${DIR}/sdirkmethodoflines.h
+  ${DIR}/sdirkmethodoflines.cc
+  ${DIR}/sdirkmethodoflines_ode.h
+  ${DIR}/sdirkmethodoflines_ode.cc
+)
+
+set(LIBRARIES
+  Eigen3::Eigen
+  LF::lf.assemble
+  LF::lf.geometry
+  LF::lf.io
+  LF::lf.mesh.hybrid2d
+  LF::lf.mesh.test_utils
+  LF::lf.mesh.utils
+  LF::lf.refinement
+  LF::lf.uscalfe
+)
