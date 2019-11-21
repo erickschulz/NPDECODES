60cad40c6f291dc8fd294375e5af778c9d9e6617
Oliver Rietmann <rioliver@student.ethz.ch>
2019-11-21 21:04:09 +0100

    Added developers/PointEvaluationRhs/

---
+++ developers/PointEvaluationRhs/mastersolution/dependencies.cmake
@@ -0,0 +1,28 @@
+#if SOLUTION
+# Dependencies of mastersolution:
+#else
+# Add your custom dependencies here:
+#endif
+
+# DIR will be provided by the calling file.
+
+set(SOURCES
+  ${DIR}/PointEvaluationRhs_main.cc
+  ${DIR}/pointEvaluation.h
+  ${DIR}/pointEvaluation.cc
+  ${DIR}/norms.h
+  ${DIR}/norms.cc
+)
+
+set(LIBRARIES
+  Eigen3::Eigen
+  LF::lf.assemble
+  LF::lf.base
+  LF::lf.geometry
+  LF::lf.io
+  LF::lf.mesh.hybrid2d
+  LF::lf.mesh.test_utils
+  LF::lf.mesh.utils
+  LF::lf.refinement
+  LF::lf.uscalfe
+)
