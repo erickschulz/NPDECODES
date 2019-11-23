c9549433911126632964e1864807bc50003d661b
Oliver Rietmann <oliver.rietmann@sam.math.ethz.ch>
2019-11-23 11:35:34 +0100

    Fixed typo in cmake/modules/build_variables.cmake

--- cmake/modules/build_variables.cmake
+++ cmake/modules/build_variables.cmake
@@ -5,4 +5,4 @@ get_filename_component(PROBLEM_NAME ${CMAKE_CURRENT_SOURCE_DIR} NAME)
 add_definitions(-DCURRENT_SOURCE_DIR=\"${CMAKE_CURRENT_SOURCE_DIR}\")
 
 # Provides a variable CURRENT_BINARY_DIR containing the path to the binary output directory.
-add_definitions(-DCURRENT_BINARY_DIR=\"${CMAKE_BINARY_SOURCE_DIR}\")
+add_definitions(-DCURRENT_BINARY_DIR=\"${CMAKE_CURRENT_BINARY_DIR}\")
