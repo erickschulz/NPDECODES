6aa20231b728948f367fe75d0b5039ab8b256763
Oliver Rietmann <rioliver@student.ethz.ch>
2019-11-25 17:26:17 +0100

    polish of DebuggingFEM

--- developers/DebuggingFEM/mastersolution/main.cc
+++ developers/DebuggingFEM/mastersolution/main.cc
@@ -6,8 +6,11 @@
  * @copyright Developed at ETH Zurich
  */
 
+#include <cstdlib>
+#include <fstream>
 #include <functional>
 #include <iomanip>
+#include <memory>
 
 #include "local_laplace_qfe.h"
 #include "qfe_interpolator.h"
@@ -22,7 +25,7 @@ using size_type = lf::base::size_type;
 int main() {
   // read mesh
   auto mesh_factory = std::make_unique<lf::mesh::hybrid2d::MeshFactory>(2);
-  lf::io::GmshReader reader(std::move(mesh_factory), CURRENT_SOURCE_DIR"/meshes/square_64.msh");
+  lf::io::GmshReader reader(std::move(mesh_factory), CURRENT_SOURCE_DIR "/meshes/square_64.msh");
   auto mesh = reader.mesh();
 
   // refine mesh
@@ -30,84 +33,77 @@ int main() {
   std::shared_ptr<lf::refinement::MeshHierarchy> multi_mesh_p =
       lf::refinement::GenerateMeshHierarchyByUniformRefinemnt(mesh, reflevels);
   lf::refinement::MeshHierarchy &multi_mesh{*multi_mesh_p};
-  size_type L = multi_mesh.NumLevels();
+  lf::base::size_type L = multi_mesh.NumLevels();
 
-  // function to interpolateOntoQuadFE
-  auto f = [](Eigen::VectorXd x) -> double {
-    return std::exp(x(1) * x(1) + x(0) * x(0));
-  };
+  // vector holding pointers to the different element matrix providers
+  std::vector<std::unique_ptr<DebuggingFEM::EntityMatrixProvider>> element_matrix_provider;
+  element_matrix_provider.emplace_back(std::make_unique<DebuggingFEM::LocalLaplaceQFE1>());
+  element_matrix_provider.emplace_back(std::make_unique<DebuggingFEM::LocalLaplaceQFE2>());
+  element_matrix_provider.emplace_back(std::make_unique<DebuggingFEM::LocalLaplaceQFE3>());
+  int num_emp = element_matrix_provider.size();
 
   // vectors accumulating the computed errors
-  std::vector<double> H1SMerr_1(L);
-  std::vector<double> H1SMerr_2(L);
-  std::vector<double> H1SMerr_3(L);
-  std::vector<double> ndofs;
+  Eigen::MatrixXd H1SMerr(L, num_emp);
+  Eigen::VectorXi N(L);
 
   for (int level = 0; level < L; ++level) {
     // set up fespace and dof handler for the mesh at the current level
     auto mesh_p = multi_mesh.getMesh(level);
-    lf::assemble::UniformFEDofHandler dofh(mesh_p,
+    const lf::assemble::UniformFEDofHandler dofh(mesh_p,
                                            {{lf::base::RefEl::kPoint(), 1},
                                             {lf::base::RefEl::kSegment(), 1},
                                             {lf::base::RefEl::kTria(), 0},
                                             {lf::base::RefEl::kQuad(), 1}});
-    // Dimension of finite element space`
-    const size_type N_dofs(dofh.NumDofs());
-    // Matrix in triplet format holding Galerkin for LocalLaplaceQFE1 matrix,
-    // zero initially.
-    lf::assemble::COOMatrix<double> A_1(N_dofs, N_dofs);
-    auto element_matrix_provider_1 = DebuggingFEM::LocalLaplaceQFE1();
-    DebuggingFEM::QFEProviderTester qfe_provider_tester_1(
-        dofh, element_matrix_provider_1);
 
-    // Matrix in triplet format holding Galerkin for LocalLaplaceQFE2 matrix,
-    // zero initially.
-    lf::assemble::COOMatrix<double> A_2(N_dofs, N_dofs);
-    auto element_matrix_provider_2 = DebuggingFEM::LocalLaplaceQFE2();
-    DebuggingFEM::QFEProviderTester qfe_provider_tester_2(
-        dofh, element_matrix_provider_2);
+    // Dimension of finite element space
+    N[level] = dofh.NumDofs();
 
-    // Matrix in triplet format holding Galerkin for LocalLaplaceQFE3 matrix,
-    // zero initially.
-    lf::assemble::COOMatrix<double> A_3(N_dofs, N_dofs);
-    auto element_matrix_provider_3 = DebuggingFEM::LocalLaplaceQFE3();
-    DebuggingFEM::QFEProviderTester qfe_provider_tester_3(
-        dofh, element_matrix_provider_3);
+    // compute error for each element matrix provider
+    for (int i = 0; i < num_emp; ++i) {
+      // function to interpolateOntoQuadFE
+      auto f = [](const Eigen::VectorXd &x) -> double {
+        return std::exp(x(1) * x(1) + x(0) * x(0));
+      };
+      // Matrix in triplet format holding Galerkin for LocalLaplaceQFEX matrix,
+      // zero initially.
+      DebuggingFEM::QFEProviderTester qfe_provider_tester(dofh, *element_matrix_provider[i]);
 
-    // interpolateOntoQuadFE exact solution
-    Eigen::VectorXd u_exact = DebuggingFEM::interpolateOntoQuadFE(dofh, f);
-
-    ndofs.push_back(N_dofs);
-
-    // compute the energies
+      // compute the energy / error
 #if SOLUTION
-    double energy_1 = qfe_provider_tester_1.energyOfInterpolant(f);
-    double h1s_error_1 = std::abs(23.76088 - energy_1);
-    H1SMerr_1[level] = h1s_error_1;
-
-    double energy_2 = qfe_provider_tester_2.energyOfInterpolant(f);
-    double h1s_error_2 = std::abs(23.76088 - energy_2);
-    H1SMerr_2[level] = h1s_error_2;
-
-    double energy_3 = qfe_provider_tester_3.energyOfInterpolant(f);
-    double h1s_error_3 = std::abs(23.76088 - energy_3);
-    H1SMerr_3[level] = h1s_error_3;
+      double energy = qfe_provider_tester.energyOfInterpolant(f);
+      H1SMerr(level, i) = std::abs(23.76088 - energy);
 #else
-  //====================
-  // Your code goes here
-  //====================
+      //====================
+      // Your code goes here
+      //====================
 #endif
+
+    }
+
   }
 
   // Tabular output of the results
   std::cout << std::left << std::setw(10) << "N" << std::setw(20)
             << "Assember 1" << std::setw(20) << "Assembler 2" << std::setw(20)
             << "Assembler 3" << std::endl;
-  for (int l = 0; l < L; ++l) {
-    std::cout << std::left << std::setw(10) << ndofs[l] << std::setw(20)
-              << H1SMerr_1[l] << std::setw(20) << H1SMerr_2[l] << std::setw(20)
-              << H1SMerr_3[l] << std::endl;
+  for (int level = 0; level < L; ++level) {
+    std::cout << std::left << std::setw(10) << N[level] << std::setw(20)
+              << H1SMerr(level, 0) << std::setw(20) << H1SMerr(level, 1) << std::setw(20)
+              << H1SMerr(level, 2) << std::endl;
   }
+
+  /*// Write .csv file and plot it by a python script
+  Eigen::MatrixXd data(L, num_emp + 1);
+  data.col(0) = N.cast<double>();
+  data.rightCols(num_emp) = H1SMerr;
+  const static Eigen::IOFormat CSVFormat(Eigen::FullPrecision,
+                                         Eigen::DontAlignCols, ", ", "\n");
+  std::ofstream error_file;
+  error_file.open("error.csv");
+  error_file << data.format(CSVFormat) << std::endl;
+  error_file.close();
+  std::cout << "Generated " CURRENT_BINARY_DIR "/error.csv" << std::endl;
+  std::system("python3 " CURRENT_SOURCE_DIR "/mastersolution/plot_error.py " CURRENT_BINARY_DIR "/error.csv " CURRENT_BINARY_DIR "/error.png");*/
 
   return 0;
 }
---
+++ developers/DebuggingFEM/mastersolution/plot_error.py
@@ -0,0 +1,19 @@
+from numpy import genfromtxt
+from matplotlib.pyplot import figure, legend, loglog, savefig, xlabel, ylabel
+from sys import argv
+
+input_file = str(argv[1])
+output_file = str(argv[2])
+
+data = genfromtxt(input_file, delimiter=',')
+N = data[:,0]
+
+fig = figure()
+for i in range(len(data[0]) - 1):
+  loglog(N, data[:,i], 'o-', label='Assembler ' + str(i))
+xlabel('Num dofs')
+ylabel('error')
+legend()
+savefig(output_file)
+
+print('Generated ' + output_file)
