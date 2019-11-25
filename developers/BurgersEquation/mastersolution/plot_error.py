07f5f982678a3d5a4d0daa2d8b2e91311459a205
Oliver Rietmann <rioliver@student.ethz.ch>
2019-11-25 14:37:03 +0100

    Changed imports in python script

--- developers/BurgersEquation/mastersolution/plot_error.py
+++ developers/BurgersEquation/mastersolution/plot_error.py
@@ -1,24 +1,24 @@
-import numpy as np
-import matplotlib.pyplot as plt
+from numpy import genfromtxt
+from matplotlib.pyplot import figure, legend, loglog, savefig, xlabel, ylabel
 from sys import argv
 
 input_file = str(argv[1])
 output_file = str(argv[2])
 
-data = np.genfromtxt(input_file, delimiter=',')
+data = genfromtxt(input_file, delimiter=',')
 h = data[0]
 error_short = data[1]
 error_long = data[2]
 
 x = [0.01, 0.1]
 
-fig = plt.figure()
-plt.loglog(h, error_short, 'o-', label='T = 0.3')
-plt.loglog(h, error_long, 'o-', label='T = 3.0')
-plt.loglog(x, x, '--', label='slope 1')
-plt.xlabel('mesh-width h')
-plt.ylabel('error')
-plt.legend()
-plt.savefig(output_file)
+fig = figure()
+loglog(h, error_short, 'o-', label='T = 0.3')
+loglog(h, error_long, 'o-', label='T = 3.0')
+loglog(x, x, '--', label='slope 1')
+xlabel('mesh-width h')
+ylabel('error')
+legend()
+savefig(output_file)
 
 print('Generated ' + output_file)
