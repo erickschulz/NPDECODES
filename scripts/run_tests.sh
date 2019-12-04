954446bb7a61ceef2d425f72e8e98bcc1a581e32
Oliver Rietmann <rioliver@student.ethz.ch>
2019-12-04 20:37:05 +0100

    slight changes in test script

---
+++ scripts/run_tests.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+EXIT_CODE=0
+FAILED_TESTS=()
+
+for TEST_FILE in $@; do
+  echo "Running $TEST_FILE"
+  "$TEST_FILE"
+  if [ $? -ne 0 ]; then
+    PROBLEM_NAME=${TEST_FILE##*/}
+    FAILED_TESTS+=("$PROBLEM_NAME")
+    EXIT_CODE=1
+  fi
+done
+
+#for TEST_FILE in $@; do
+#    echo "Running $TEST_FILE"
+#    "$TEST_FILE"
+#    if [ $? -ne 0 ]; then
+#        PROBLEM_NAME=${TEST_FILE##*/}
+#        FAILED_TESTS+=("$PROBLEM_NAME")
+#        EXIT_CODE=1
+#    fi
+#done
+
+if [ $EXIT_CODE -ne 0 ]; then
+  echo "The following tests have failed: "
+  for PROBLEM_NAME in ${FAILED_TESTS[@]}; do
+    echo " - $PROBLEM_NAME"
+  done
+else
+  echo " ALL TESTS HAVE PASSED"
+fi
