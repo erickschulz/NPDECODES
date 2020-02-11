02ef977b0513960e8149fe3a46b348c13090f425
Oliver Rietmann <rioliver@student.ethz.ch>
2020-02-11 17:00:23 +0100

    Removed Boost

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -1,7 +1,4 @@
 # This file specifies additional data for the dependencies that are imported via hunter
-
-hunter_config(Boost VERSION 1.66.0)
-
 hunter_config(Eigen VERSION 3.3.5)
 
 #hunter_config(lehrfempp VERSION 0.7.20)
