3447cde2d30be23c0b3547b73b92603161d73ee0
Erick <erick.schulz@sam.math.ethz.ch>
2020-02-19 14:45:43 +0100

    Added problem ZienkiewiczZhuEstimator

---
+++ developers/ZienkiewiczZhuEstimator/mastersolution/dependencies.cmake
@@ -0,0 +1,26 @@
+#if SOLUTION
+# Dependencies of mastersolution:
+#else
+# Add your custom dependencies here:
+#endif
+
+# DIR will be provided by the calling file.
+
+set(SOURCES
+  ${DIR}/ZienkiewiczZhuEstimator_main.cc
+  ${DIR}/ZienkiewiczZhuEstimator.h
+  ${DIR}/ZienkiewiczZhuEstimator.cc
+)
+
+set(LIBRARIES
+  Eigen3::Eigen
+  LF::lf.assemble
+  LF::lf.base
+  LF::lf.geometry
+  LF::lf.io
+  LF::lf.mesh.hybrid2d
+  LF::lf.mesh.test_utils
+  LF::lf.mesh.utils
+  LF::lf.refinement
+  LF::lf.uscalfe
+)
