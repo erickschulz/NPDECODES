19e17c38b053cef9b86ab74e33bc651c0e60a872
craffael <raffael@casagrande.ch>
2020-02-20 21:42:18 +0100

    Use boost 1.66 explicitly to avoid osx compilation failure for xcode 10.2

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -1,5 +1,6 @@
 # This file specifies additional data for the dependencies that are imported via hunter
 hunter_config(Eigen VERSION 3.3.5)
+hunter_config(Boost VERSION 1.66.0)
 
 #hunter_config(lehrfempp VERSION 0.7.20)
 hunter_config(lehrfempp
