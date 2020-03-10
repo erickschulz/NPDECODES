a672134bd283b817087fe027181a00ea3d44d918
Oliver Rietmann <oliver.rietmann@sam.math.ethz.ch>
2020-03-10 20:56:40 +0100

    Rename RegularizedNeumann --> RegularizedNeumannProblem

---
+++ developers/RegularizedNeumannProblem/mastersolution/regularizedneumannproblem_main.cc
@@ -0,0 +1,39 @@
+/**
+ * @file regularizedneumannproblem_main.cc
+ * @brief NPDE homework RegularizedNeumannProblem code
+ * @author Christian Mitsch, Philippe Peter
+ * @date March 2020
+ * @copyright Developed at ETH Zurich
+ */
+
+#include <iostream>
+#include <memory>
+
+#include <Eigen/Core>
+
+#include <lf/mesh/test_utils/test_meshes.h>
+#include <lf/mesh/utils/utils.h>
+#include <lf/uscalfe/uscalfe.h>
+
+#include "regularizedneumannproblem.h"
+
+int main() {
+
+  std::cout << "You can use the mainfile to call your functions" << std::endl;
+
+  const auto f = lf::mesh::utils::MeshFunctionGlobal(
+      [](Eigen::Vector2d x) -> double { return 1.0; });
+  const auto h = lf::mesh::utils::MeshFunctionGlobal(
+      [](Eigen::Vector2d x) -> double { return 1.0; });
+
+  auto mesh_p = lf::mesh::test_utils::GenerateHybrid2DTestMesh(4, 3.0);
+
+  auto fe_space =
+      std::make_shared<lf::uscalfe::FeSpaceLagrangeO1<double>>(mesh_p);
+
+  // Compute solution
+  auto result_c = RegularizedNeumannProblem::getGalerkinLSE_dropDof(fe_space, f, h);
+  auto result_f = RegularizedNeumannProblem::getGalerkinLSE_augment(fe_space, f, h);
+
+  return 0;
+}
---
+++ homeworks/RegularizedNeumannProblem/mastersolution/test/dependencies.cmake
@@ -0,0 +1,20 @@
+# Dependencies of mastersolution tests:
+
+# PROBLEM_NAME and DIR will be provided by the calling file.
+
+set(SOURCES
+  ${DIR}/test/regularizedneumannproblem_test.cc
+)
+
+set(LIBRARIES
+  Eigen3::Eigen
+  GTest::gtest_main
+  LF::lf.assemble
+  LF::lf.base
+  LF::lf.geometry
+  LF::lf.io
+  LF::lf.mesh
+  LF::lf.mesh.hybrid2d
+  LF::lf.mesh.utils
+  LF::lf.uscalfe
+)
