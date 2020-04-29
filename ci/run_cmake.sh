e6bd995482eda20bdc3aa2cfb8dc13fcc2c9c716
craffael <raffael@casagrande.ch>
2020-04-29 10:47:33 +0200

    introduce hunter stage

--- ci/run_cmake.sh
+++ ci/run_cmake.sh
@@ -4,7 +4,8 @@
 #   COMPILER specifies the compiler to use (e.g. g++-7)
 #   BUILD_TYPE specifies the CMAKE_BUILD_TYPE.
 # 
-# It builds NPDECODES using the provided compiler + cmake configuration.
+# It configures and generates the makefiles for NPDECODES using cmake
+# and the provided COMPILER + BUILD_TYPE
 
 # Exit immediately from this script upon error
 set -e
@@ -15,7 +16,7 @@ mkdir -p ${HUNTER_ROOT}
 # Install cmake
 source $(dirname $0)/install_cmake.sh
 
-# compile
+# create build directory and run cmake.
 cd ${TRAVIS_BUILD_DIR}
 export CXX=${COMPILER}
 $CXX --version
