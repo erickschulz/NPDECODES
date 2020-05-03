6c3b9ebbe5d559e1f55fd785f3618c67379e119a
Oliver Rietmann <oliver.rietmann@sam.math.ethz.ch>
2020-05-03 22:32:49 +0200

    Nicer norm plot

--- developers/NonLinSchroedingerEquation/mastersolution/plot_norm.py
+++ developers/NonLinSchroedingerEquation/mastersolution/plot_norm.py
@@ -1,5 +1,5 @@
-from matplotlib.pyplot import figure, legend, plot, savefig, title, xlabel, ylabel
-from numpy import genfromtxt
+from matplotlib.pyplot import figure, plot, savefig, title, xlabel, ylabel, yticks
+from numpy import genfromtxt, linspace
 from sys import argv
 
 input_file = str(argv[1])
@@ -11,10 +11,10 @@ norm = data[1]
 
 fig = figure()
 title('Norm of the Approximate Solution')
-plot(t, norm, '-', label='norm')
+plot(t, norm, '-')
 xlabel('t')
-ylabel(r"$\vert u(t)\vert_{L^2(\mathbb{R}^2;\mathbb{C})})$") #
-legend()
+ylabel(r"$\Vert u(t)\Vert_{L^2(\mathbb{R}^2;\mathbb{C})}$")
+yticks(linspace(0.0, 2.0 * max(norm), 11, endpoint=True))
 savefig(output_file)
 
 print('Generated ' + output_file)
