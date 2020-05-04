525c9ff65185a40b9f6940d96e713d8775a356c3
Oliver Rietmann <oliver.rietmann@sam.math.ethz.ch>
2020-05-04 14:57:07 +0200

    Added solution tags

--- developers/NonLinSchroedingerEquation/mastersolution/nonlinschroedingerequation.cc
+++ developers/NonLinSchroedingerEquation/mastersolution/nonlinschroedingerequation.cc
@@ -36,16 +36,43 @@ Eigen::Matrix3d MassElementMatrixProvider::Eval(const lf::mesh::Entity &cell) {
 
 /* SAM_LISTING_BEGIN_2 */
 double Norm(const Eigen::VectorXcd &mu, const Eigen::SparseMatrix<double> &D) {
+#if SOLUTION
   return std::sqrt(mu.dot(D * mu).real());
+#else
+  //====================
+  // Your code goes here
+  // Replace this dummy value by the approximate
+  // norm of the mesh function assiciated with mu
+  return 1.0;
+  //====================
+#endif
 }
 
 double KineticEnergy(const Eigen::VectorXcd &mu, const Eigen::SparseMatrix<double> &A) {
+#if SOLUTION
   return 0.5 * mu.dot(A * mu).real();
+#else
+  //====================
+  // Your code goes here
+  // Replace this dummy value by the kinetic energy
+  // of the mesh function assiciated with mu
+  return 0.0;
+  //====================
+#endif
 }
 
 double InteractionEnergy(const Eigen::VectorXcd &mu, const Eigen::SparseMatrix<double> &D) {
+#if SOLUTION
   Eigen::VectorXd mu_abs2 = mu.cwiseAbs2();
   return 0.25 * mu_abs2.dot(D * mu_abs2);
+#else
+  //====================
+  // Your code goes here
+  // Replace this dummy value by the interaction
+  // energy of the mesh function assiciated with mu
+  return 0.0;
+  //====================
+#endif
 }
 /* SAM_LISTING_END_2 */
 
--- developers/NonLinSchroedingerEquation/mastersolution/propagator.cc
+++ developers/NonLinSchroedingerEquation/mastersolution/propagator.cc
@@ -18,26 +18,60 @@
 namespace NonLinSchroedingerEquation {
 
 // KineticPropagator
+/* SAM_LISTING_BEGIN_1 */
 KineticPropagator::KineticPropagator(const SparseMatrixXd &A, const SparseMatrixXcd &M, double tau) {
+#if SOLUTION
   B_plus_ = M + 0.5 * tau * A.cast<std::complex<double>>();
   SparseMatrixXcd B_minus = M - 0.5 * tau * A.cast<std::complex<double>>();
   solver_.compute(B_minus);
+#else
+  //====================
+  // Your code goes here
+  //====================
+#endif
 }
 
 Eigen::VectorXcd KineticPropagator::operator()(const Eigen::VectorXcd &mu) const {
+#if SOLUTION
   return solver_.solve(B_plus_ * mu);
+#else
+  //====================
+  // Your code goes here
+  // Replace mu by its value after a timestep tau
+  return mu;
+  //====================
+#endif
 }
+/* SAM_LISTING_END_1 */
 
 // InteractionPropagator
+/* SAM_LISTING_BEGIN_2 */
 InteractionPropagator::InteractionPropagator(double tau) {
+#if SOLUTION
   phase_multiplier_ = [tau] (std::complex<double> z) {
     const std::complex<double> i(0, 1);
     return std::exp(-i * tau * std::norm(z)) * z;
   };
+#else
+  //====================
+  // Your code goes here
+  // Replace this by the proper phase multiplier
+  return [] (std::complex<double> z) { return z; };
+  //====================
+#endif
 }
 
 Eigen::VectorXcd InteractionPropagator::operator()(const Eigen::VectorXcd &mu) const {
+#if SOLUTION
   return mu.unaryExpr(phase_multiplier_);
+#else
+  //====================
+  // Your code goes here
+  // Replace mu by its value after a timestep tau
+  return mu;
+  //====================
+#endif
 }
+/* SAM_LISTING_END_2 */
 
 }  // namespace NonLinSchroedingerEquation
