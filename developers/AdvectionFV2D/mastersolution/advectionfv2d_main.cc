838b18091280b88e9400f809b71cb241f5838976
ralfh <ralfh@ethz.ch>
2020-06-25 07:39:23 +0200

    Removed ILLEGAL dynamic local arrays in AdvectionFV2D!

--- developers/AdvectionFV2D/mastersolution/advectionfv2d_main.cc
+++ developers/AdvectionFV2D/mastersolution/advectionfv2d_main.cc
@@ -6,10 +6,10 @@
  * @copyright Developed at ETH Zurich
  */
 
-#include <iostream>
-#include <fstream>
 #include <algorithm>
+#include <fstream>
 #include <functional>
+#include <iostream>
 
 #include <Eigen/Core>
 #include <Eigen/SparseCore>
@@ -23,24 +23,25 @@
 #include <lf/mesh/mesh.h>
 #include <lf/mesh/test_utils/test_meshes.h>
 #include <lf/mesh/utils/utils.h>
-#include <lf/uscalfe/uscalfe.h>
 #include <lf/refinement/refinement.h>
+#include <lf/uscalfe/uscalfe.h>
 
 #include "advectionfv2d.h"
 
 int main() {
-	
-  //TODO inconsistancy: g vs. G
-  auto mesh_p = lf::mesh::test_utils::GenerateHybrid2DTestMesh(0, 1./3.);
-  
-  auto beta = [](Eigen::Vector2d x) -> Eigen::Vector2d { 
-      return Eigen::Vector2d(-x[1], x[0]); };
-  
-  // Perferforming some manual tests for computeCellNormals and 
+
+  // TODO inconsistancy: g vs. G
+  auto mesh_p = lf::mesh::test_utils::GenerateHybrid2DTestMesh(0, 1. / 3.);
+
+  auto beta = [](Eigen::Vector2d x) -> Eigen::Vector2d {
+    return Eigen::Vector2d(-x[1], x[0]);
+  };
+
+  // Perferforming some manual tests for computeCellNormals and
   // getAdjacentCellPointers (Tests were OK)
   /*
   std::shared_ptr<lf::mesh::utils::CodimMeshDataSet
-      <Eigen::Matrix<double,2,Eigen::Dynamic>>> 
+      <Eigen::Matrix<double,2,Eigen::Dynamic>>>
       normal_vectors = AdvectionFV2D::computeCellNormals(mesh_p);
 
   std::shared_ptr<lf::mesh::utils::CodimMeshDataSet
@@ -51,138 +52,141 @@ int main() {
   for (const lf::mesh::Entity *cell : mesh_p->Entities(0)) {
     const lf::geometry::Geometry* geo_p = cell->Geometry();
     const Eigen::MatrixXd corners = lf::geometry::Corners(*geo_p);
-    
+
     Eigen::Vector2d cur_midpoint = AdvectionFV2D::barycenter(corners);
-    
+
     std::cout << "Element NR: " << element_idx << std::endl;
     std::cout << "Corners: \n" << corners << std::endl;
     std::cout << "Barycenter: \n" << cur_midpoint << std::endl;
     std::cout << "Normal Vectors\n" << (*normal_vectors)(*cell) << std::endl;
-    
+
     for (const lf::mesh::Entity* neighbour_cell: (*adjacentCells)(*cell)){
-		if (neighbour_cell != nullptr){
-		  int neighbour_ind = 0;
+                if (neighbour_cell != nullptr){
+                  int neighbour_ind = 0;
           for (const lf::mesh::Entity *cell2 : mesh_p->Entities(0)){
-			  if (neighbour_cell == cell2){
-				  std::cout << "Neighbour at: " << neighbour_ind << std::endl;
-			  }
-			  neighbour_ind++;
-		  }
-		} else {
-			std::cout << "Neighbour at: - "<< std::endl;
-		}
-	}
+                          if (neighbour_cell == cell2){
+                                  std::cout << "Neighbour at: " << neighbour_ind
+  << std::endl;
+                          }
+                          neighbour_ind++;
+                  }
+                } else {
+                        std::cout << "Neighbour at: - "<< std::endl;
+                }
+        }
     element_idx++;
   }
   */
-  
-  
+
   // Output B_Matrix
   /*
-  const lf::assemble::UniformFEDofHandler dofh(mesh_p, 
+  const lf::assemble::UniformFEDofHandler dofh(mesh_p,
       {{lf::base::RefEl::kPoint(), 0},
       {lf::base::RefEl::kSegment(), 0},
       {lf::base::RefEl::kTria(), 1},
       {lf::base::RefEl::kQuad(), 1}});
-  
-  Eigen::SparseMatrix<double> B_Matrix = 
+
+  Eigen::SparseMatrix<double> B_Matrix =
       AdvectionFV2D::initializeMOLODEMatrix(dofh, beta);
-      
+
   std::cout << "B_Matrix\n" << B_Matrix << std::endl;
   */
 
-  //Testing Output minH
+  // Testing Output minH
   /*
   double minH = AdvectionFV2D::computeHmin(mesh_p);
   std::cout << "minh " << minH << std::endl;
   */
-  
-  //Test of Task 8-8.n
-  //Eigen::VectorXd result = AdvectionFV2D::simulateAdvection(dofh);
+
+  // Test of Task 8-8.n
+  // Eigen::VectorXd result = AdvectionFV2D::simulateAdvection(dofh);
 
   // Task 8-8.o
   //////////////////////////////////////////////////////////////////////
   // TODO: Change to refinement_level = 5
   //////////////////////////////////////////////////////////////////////
   // Generate a mesh hierarchy
-  auto mesh_seq_p{ 
-      lf::refinement::GenerateMeshHierarchyByUniformRefinemnt(mesh_p,4)};
+  auto mesh_seq_p{
+      lf::refinement::GenerateMeshHierarchyByUniformRefinemnt(mesh_p, 4)};
   int num_meshes = mesh_seq_p->NumLevels();
-  
-  int array_num_cells[num_meshes] = {};
-  double array_l2error[num_meshes] = {};
-  
+
+  // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+  // Variable-size local arrays must not be used in C++.
+  // https://stackoverflow.com/questions/7812566/why-no-variable-size-array-in-stack
+  // Use std::vector
+  // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+  int array_num_cells[num_meshes];
+  double array_l2error[num_meshes];
+
   // Iterate over all mesh levels
-  for (int level = 0; level < num_meshes; level++){
+  for (int level = 0; level < num_meshes; level++) {
     std::cout << "Computing L2Error for level: " << level << std::endl;
-    
+
     // Get the current mesh
     auto cur_mesh = mesh_seq_p->getMesh(level);
-	  
+
     // Create a DOF Hander for the current mesh
-    const lf::assemble::UniformFEDofHandler cur_dofh(cur_mesh, 
-        {{lf::base::RefEl::kPoint(), 0},
-        {lf::base::RefEl::kSegment(), 0},
-        {lf::base::RefEl::kTria(), 1},
-        {lf::base::RefEl::kQuad(), 1}});
-	
-    //Get Result from Simulation
+    const lf::assemble::UniformFEDofHandler cur_dofh(
+        cur_mesh, {{lf::base::RefEl::kPoint(), 0},
+                   {lf::base::RefEl::kSegment(), 0},
+                   {lf::base::RefEl::kTria(), 1},
+                   {lf::base::RefEl::kQuad(), 1}});
+
+    // Get Result from Simulation
     Eigen::VectorXd result = AdvectionFV2D::simulateAdvection(cur_dofh);
-    
-    //Get Exact Result
+
+    // Get Exact Result
     Eigen::VectorXd ref_solution = AdvectionFV2D::refSolution(cur_dofh);
-    
-    //Compute L2 Error
+
+    // Compute L2 Error
     double l2_error = 0;
     for (const lf::mesh::Entity *cell : cur_mesh->Entities(0)) {
-      const lf::geometry::Geometry* geo_p = cell->Geometry();
+      const lf::geometry::Geometry *geo_p = cell->Geometry();
       double area = lf::geometry::Volume(*geo_p);
       int idx = cur_dofh.InteriorGlobalDofIndices(*cell)[0];
-      l2_error += std::pow( (result[idx] - ref_solution[idx]), 2) * area;
+      l2_error += std::pow((result[idx] - ref_solution[idx]), 2) * area;
     }
-    
+
     // Store the results in the arrays
     array_num_cells[level] = cur_dofh.NumDofs();
     array_l2error[level] = l2_error;
   }
 
+  // Task 8-8.q
+  double array_clf_thres[num_meshes];
 
-  //Task 8-8.q
-  double array_clf_thres[num_meshes] = {};
-  
-  //Iterate over all mesh levels
-  for (int level = 0; level < num_meshes; level++){
+  // Iterate over all mesh levels
+  for (int level = 0; level < num_meshes; level++) {
     // Get the current mesh
     auto cur_mesh = mesh_seq_p->getMesh(level);
-    
+
     // Create a DOF Hander for the current mesh
-    const lf::assemble::UniformFEDofHandler cur_dofh(cur_mesh, 
-        {{lf::base::RefEl::kPoint(), 0},
-        {lf::base::RefEl::kSegment(), 0},
-        {lf::base::RefEl::kTria(), 1},
-        {lf::base::RefEl::kQuad(), 1}});
-    
-    //Save the CFL threshold to array for T = 1.0
+    const lf::assemble::UniformFEDofHandler cur_dofh(
+        cur_mesh, {{lf::base::RefEl::kPoint(), 0},
+                   {lf::base::RefEl::kSegment(), 0},
+                   {lf::base::RefEl::kTria(), 1},
+                   {lf::base::RefEl::kQuad(), 1}});
+
+    // Save the CFL threshold to array for T = 1.0
     std::cout << "Computing threshold for level: " << level << std::endl;
-    array_clf_thres[level] = 
+    array_clf_thres[level] =
         AdvectionFV2D::findCFLthreshold(cur_dofh, beta, 1.0);
   }
-  
-  //Write Output file of Task 8-8.o and 8-8.q
+
+  // Write Output file of Task 8-8.o and 8-8.q
   std::ofstream csv_file;
-  csv_file.open ("advectionfv2d.csv");
-  for (int level = 0; level < num_meshes; level++){
-    std::cout << "Cells: "<< array_num_cells[level] 
-              << " | Error: " << array_l2error[level] 
-              << std::endl;
-    csv_file << array_num_cells[level] << "," 
-             << array_l2error[level] << "," 
+  csv_file.open("advectionfv2d.csv");
+  for (int level = 0; level < num_meshes; level++) {
+    std::cout << "Cells: " << array_num_cells[level]
+              << " | Error: " << array_l2error[level] << std::endl;
+    csv_file << array_num_cells[level] << "," << array_l2error[level] << ","
              << array_clf_thres[level] << "\n";
   }
   csv_file.close();
 
-  //const static Eigen::IOFormat CSVFormat(Eigen::FullPrecision,
+  // const static Eigen::IOFormat CSVFormat(Eigen::FullPrecision,
   //    Eigen::DontAlignCols, ", ", "\n");
-  //std::cout << v.transpose().format(CSVFormat) << std::endl;
+  // std::cout << v.transpose().format(CSVFormat) << std::endl;
   return 0;
 }
