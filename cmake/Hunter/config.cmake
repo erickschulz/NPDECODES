0f526ff575828fe784d55d43721e7077a12490ea
craffael <raffael@casagrande.ch>
2020-07-31 23:13:00 +0200

    Use latest version of lehrfempp

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -3,10 +3,10 @@ hunter_config(Eigen VERSION 3.3.5)
 hunter_config(Boost VERSION 1.66.0)
 
 hunter_config(lehrfempp VERSION 0.7.21)
-# hunter_config(lehrfempp
-  # # Currently using commit 'Update README.md'
-  # URL "https://github.com/craffael/lehrfempp/archive/c50c2cebe1aa3eb4db0c899e8fcdb196445820f5.tar.gz"
-  # SHA1 "df5748205b66a0fd74c81978b04ccd503509cfe6"
-# )
+hunter_config(lehrfempp
+  # Currently using commit 'Update README.md'
+  URL "https://github.com/craffael/lehrfempp/archive/d36d54238ac0035243a924a3988642b70614d7ba.tar.gz"
+  SHA1 "a113e5315e2b1c588684d3e812f5a3adc8954f5a"
+)
 
 hunter_config(GTest VERSION 1.10.0)
