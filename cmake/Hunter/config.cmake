56619fce732cacb39c051464f49012c9c78186fa
craffael <raffael@casagrande.ch>
2020-10-19 12:14:08 +0200

    remove comment in Hunter/config.cmake

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -4,7 +4,6 @@ hunter_config(Eigen VERSION 3.3.8-b11f817)
 
 # hunter_config(lehrfempp VERSION 0.7.21)
 hunter_config(lehrfempp
-  # Currently using commit 'Update README.md'
   URL "https://github.com/craffael/lehrfempp/archive/20b6c6e0e20808947b4baa9e86d32274a677f731.tar.gz"
   SHA1 "bcde35540683d5fe929a4aa16baf241c0fa00350"
 )
