702a5039b3ef5fe33239a97d43fe8b934c06337f
craffael <raffael@casagrande.ch>
2020-10-21 07:38:14 +0200

    update lehrfem++ version since issue 184 has been merged

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -4,8 +4,8 @@ hunter_config(Eigen VERSION 3.3.8-b11f817)
 
 # hunter_config(lehrfempp VERSION 0.7.21)
 hunter_config(lehrfempp
-  URL "https://github.com/craffael/lehrfempp/archive/20b6c6e0e20808947b4baa9e86d32274a677f731.tar.gz"
-  SHA1 "bcde35540683d5fe929a4aa16baf241c0fa00350"
+  URL "https://github.com/craffael/lehrfempp/archive/bf7c8bc2c8c06910a833d2ac010d9d48be3fdaed.tar.gz"
+  SHA1 "ee5527d403ff4cc03caa83c691caaaaf033bf75d"
 )
 
 hunter_config(GTest VERSION 1.10.0)
