e31e428706c281c16b8ec415e74d6ab3979baf81
Philippe Peter <phpeter@student.ethz.ch>
2021-01-03 19:31:27 +0100

    Implemented Tests for Bernoulli Function

--- developers/ExpFittedUpwind/mastersolution/test/expfittedupwind_test.cc
+++ developers/ExpFittedUpwind/mastersolution/test/expfittedupwind_test.cc
@@ -13,9 +13,20 @@
 
 namespace ExpFittedUpwind::test {
 
-TEST(Bernoulli, Cancellation) {
-    std::vector<double> values = {3.0 , 1.5, 0.5 ,1.0E-5, 1.0E-7, 1.0E-9  };
+TEST(Bernoulli, Monotinicity) {
+    std::vector<double> values = {3.0 , 1.5, 0.5 ,1.0E-5, 1.0E-7, 1.0E-9, -1.0E-9, -1.0E-7, -1.0E-5, -1.0, -5.0, -6.0};
+
+    for(int i = 0; i< values.size()-1; ++i){
+        EXPECT_LE(Bernoulli(values[i]), Bernoulli(values[i+1]));
+    }
 }
+
+TEST(Bernoulli, Cancellation){
+    for(int i = 8.0; i >= 10E-12; i/=2.0){
+        EXPECT_LE(Bernoulli(i), 2.0);
+    }
+}
+
 
 }  // namespace OutputImpedanceBVP::test
 
