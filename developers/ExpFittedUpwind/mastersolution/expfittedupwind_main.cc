54fd29819b32adaeec67296f4b0e1b9383f027a5
Philippe Peter <phpeter@student.ethz.ch>
2021-01-07 21:16:53 +0100

    fixed namespace bug

--- developers/ExpFittedUpwind/mastersolution/expfittedupwind_main.cc
+++ developers/ExpFittedUpwind/mastersolution/expfittedupwind_main.cc
@@ -8,6 +8,7 @@
 
 #include <cmath>
 #include <cstdlib>
+#include <fstream>
 #include <iostream>
 #include <memory>
 
@@ -48,7 +49,7 @@ int main() {
   unsigned int reflevels = 6;
   std::unique_ptr<lf::mesh::MeshFactory> mesh_factory_ptr =
       std::make_unique<lf::mesh::hybrid2d::MeshFactory>(2);
-  lf::mesh::hybrid2d::TPTriagMeshBuilder builder(std::move(mesh_factory_ptr));
+  lf::mesh::utils::TPTriagMeshBuilder builder(std::move(mesh_factory_ptr));
   builder.setBottomLeftCorner(Eigen::Vector2d{0.0, 0.0})
       .setTopRightCorner(Eigen::Vector2d{1.0, 1.0})
       .setNumXCells(2)
