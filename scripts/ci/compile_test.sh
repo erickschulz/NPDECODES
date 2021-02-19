c2685cac19ee5592f0fc806847a347c70a76a8be
craffael <raffael@casagrande.ch>
2021-02-19 17:44:38 +0100

    improved version...

--- scripts/ci/compile_test.sh
+++ scripts/ci/compile_test.sh
@@ -9,7 +9,8 @@ set -e
 for d in ./developers/*/ ;
 do
   [[ $d =~ ./developers/(.*)/ ]]
-  cmd="./${BASH_REMATCH[1]}_test_mastersolution";
+  cmd_test="./${BASH_REMATCH[1]}_test_mastersolution";
+  cmd_mastersolution="./${BASH_REMATCH[1]}_mastersolution";
   
   if [[ $d =~ /CMakeFiles/ ]]; then
     continue
@@ -17,6 +18,25 @@ do
   echo "-----------------------------------------------------------------------";
   echo "Compiling & Running tests in $d";
   echo "-----------------------------------------------------------------------";
-  echo "Executing $cmd"
-  (cd "$d" && make -j2 && eval $cmd && make clean);
+  cd "$d";
+  make -j2;
+  # Run unit tests if exists:
+  if [[ -f "$cmd_test" ]]; then
+    echo "Executing $cmd_test";
+    eval $cmd_test
+  else
+    echo "*** WARNING: No unit tests found in $d ***";
+  fi
+  # Run solution if exists:
+  if [[ -f "$cmd_mastersolution" ]]; then
+    echo "Executing $cmd_mastersolution";
+    if ! [[ output=$(eval $cmd_mastersolution 2>&1) ]]; then
+      echo $output;
+      exit 1;
+    fi
+  else
+    echo "*** WARNING: No mastersolution found in $d ***";
+  fi
+  cd ../..;
+  # (&& make -j2 && eval $cmd && make clean);
 done
