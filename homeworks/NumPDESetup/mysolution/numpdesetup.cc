7e2a30085d756cf626c40717c5809ab5babff057
Ralf Hiptmair <hiptmair@sam.math.ethz.ch>
2021-02-28 10:49:58 +0100

    Changed a student solution for testing purposes

--- homeworks/NumPDESetup/mysolution/numpdesetup.cc
+++ homeworks/NumPDESetup/mysolution/numpdesetup.cc
@@ -17,6 +17,7 @@ namespace NumPDESetup {
 Eigen::VectorXd dummyFunction(double x, int n) {
   // Appears only in mysolution and templates
   std::cout << "NumPDESetup: student solution code" << std::endl;
+  // This is not a meaningful solution
   return Eigen::Vector2d::Zero();
 }
 /* SAM_LISTING_END_1 */
