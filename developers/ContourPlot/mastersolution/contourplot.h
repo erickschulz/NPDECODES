be9779bc594c52bf1d6dae43a0b0e9ffa240c029
Oliver Rietmann <rioliver@student.ethz.ch>
2021-03-12 17:58:40 +0100

    Integrated ContourPlot

---
+++ developers/ContourPlot/mastersolution/contourplot.h
@@ -0,0 +1,108 @@
+#include <cmath>
+#include <limits>
+#include <utility>
+#include <vector>
+
+#include <Eigen/Dense>
+
+#include "ode45.h"
+
+namespace ContourPlot {
+
+/* SAM_LISTING_BEGIN_0 */
+template <typename GradientFunctor>
+Eigen::Matrix<double, 2, Eigen::Dynamic> computeIsolinePoints(
+    GradientFunctor &&gradF, Eigen::Vector2d y0, double T) {
+  Eigen::Matrix<double, 2, Eigen::Dynamic> States;
+  // To do: (0-2.b)
+  // START
+  // Right-hand-side vector field of isoline ODE
+  auto rhs = [gradF](const Eigen::Vector2d &x) {
+    Eigen::Vector2d gradFx = gradF(x);
+    Eigen::Vector2d out(-gradFx(1), gradFx(0));  // rotate
+    out /= gradFx.norm();                        // scale
+    return out;
+  };
+  // Adaptive explicit Runge-Kutta method
+  ode45<Eigen::Vector2d> integrator(rhs);
+  // Set tolerances for timestep control
+  integrator.options.rtol = 0.00001;
+  integrator.options.atol = 1e-9;
+  // Perform explicit timestepping
+  std::vector<std::pair<Eigen::Vector2d, double>> sol = integrator.solve(y0, T);
+  // Convert output into requested format: points on isoline arranged into the
+  // columns of a matrix.
+  int N = sol.size() - 1;
+  States = Eigen::MatrixXd::Zero(2, N + 1);
+  for (int i = 0; i <= N; i++) {
+    States.col(i) = sol[i].first;
+  }
+  // END
+  return States;
+}
+/* SAM_LISTING_END_0 */
+
+/* SAM_LISTING_BEGIN_1 */
+Eigen::Matrix<double, 2, Eigen::Dynamic> crookedEgg() {
+  // crookedEggCurve will need to be reshaped to 2*(N+1).
+  Eigen::Matrix<double, 2, Eigen::Dynamic> crookedEggCurve;
+  // To do: (0-2.c)
+  // START
+  Eigen::Vector2d y0(1., 0.);
+  double T = 4.;
+  auto gradF = [](const Eigen::Vector2d &x) {
+    double x2 = x(0) * x(0), y2 = x(1) * x(1);
+    double x2y2 = 4. * (x2 + y2);
+    Eigen::Vector2d grad(x2y2 * x(0) - 3. * x2, x2y2 * x(1) - 3. * y2);
+    return grad;
+  };
+  crookedEggCurve = computeIsolinePoints(gradF, y0, T);
+  // END
+  return crookedEggCurve;
+}
+/* SAM_LISTING_END_1 */
+
+/* SAM_LISTING_BEGIN_2 */
+template <typename FFunctor>
+Eigen::Matrix<double, 2, Eigen::Dynamic> computeIsolinePointsDQ(
+    FFunctor &&F, Eigen::Vector2d y0, double T) {
+  // States will need to be reshaped to 2*(N+1).
+  Eigen::Matrix<double, 2, Eigen::Dynamic> States;
+  // To do: (0-2.d)
+  // START
+  // Get gradient of F by symmetric difference quotients with
+  // a span given by the root of the machine precision in order
+  // to curb cancellation as much as possible.
+  double epsrt = std::sqrt(std::numeric_limits<double>::epsilon());
+  auto gradF = [epsrt, F](const Eigen::Vector2d &x) {
+    Eigen::Vector2d grad, dx, dy;
+    dx << epsrt, 0.;
+    dy << 0., epsrt;
+    grad(0) = (F(x + dx) - F(x - dx)) / (2 * epsrt);
+    grad(1) = (F(x + dy) - F(x - dy)) / (2 * epsrt);
+    return grad;
+  };
+  // The remainder as in computeIsolinePoints()
+  auto rhs = [gradF](const Eigen::Vector2d &x) {
+    Eigen::Vector2d gradFx = gradF(x);
+    Eigen::Vector2d out(-gradFx(1), gradFx(0));  // rotate
+    out /= gradFx.norm();                        // scale
+    return out;
+  };
+
+  ode45<Eigen::Vector2d> integrator(rhs);
+  integrator.options.rtol = 0.00001;
+  integrator.options.atol = 1e-9;
+
+  std::vector<std::pair<Eigen::Vector2d, double>> sol = integrator.solve(y0, T);
+  int N = sol.size() - 1;
+  States = Eigen::MatrixXd::Zero(2, N + 1);
+  for (int i = 0; i <= N; i++) {
+    States.col(i) = sol[i].first;
+  }
+  // END
+  return States;
+}
+/* SAM_LISTING_END_2 */
+
+}  // namespace ContourPlot
---
+++ developers/ContourPlot/mastersolution/contourplot_main.cc
@@ -0,0 +1,137 @@
+#include <cmath>
+#include <iostream>
+
+#include <Eigen/Dense>
+
+#include "contourplot.h"
+
+int main() {
+  double tol = 1e-4;
+  // Function for crooked egg:
+  auto F = [](const Eigen::Vector2d &x) {
+    double x2 = x(0) * x(0), y2 = x(1) * x(1);
+    double x2y2 = x2 + y2;
+    return x2y2 * x2y2 - x2 * x(0) - y2 * x(1);
+  };
+  // Test function:
+  auto G = [](const Eigen::Vector2d &x) {
+    return 0.5 * x(0) * x(0) * x(1) - x(1) * x(1) * x(1) / 6;
+  };
+  // Gradient of test function:
+  auto gradG = [](const Eigen::Vector2d &x) {
+    Eigen::Vector2d grad(x(0) * x(1), 0.5 * x(0) * x(0) - 0.5 * x(1) * x(1));
+    return grad;
+  };
+  Eigen::Vector2d y0(-0.5, 0.5);
+  Eigen::Vector2d z0(1.0, 0.0);
+
+  /*
+   *  run computeIsolinePoints()
+   */
+  Eigen::MatrixXd isoline = ContourPlot::computeIsolinePoints(gradG, y0, 5.);
+  std::cout << "Output of computeIsolinePoints() is a " << isoline.rows()
+            << " by " << isoline.cols() << " matrix";
+  if (isoline.cols() > 5)
+    std::cout << ", starting with\n" << isoline.leftCols(5) << " ...\n";
+  else
+    std::cout << ":\n" << isoline << std::endl;
+
+  /*
+   *  test ContourPlot::computeIsolinePoints()
+   */
+  if (isoline.rows() == 2 && isoline.cols() >= 10) {
+    // ode45 takes at least 10 steps by default.
+    // Check initial state:
+    bool passed_computeIsolinePoints = ((y0 - isoline.col(0)).norm() < tol);
+    double Gy0 = G(y0);
+    for (int i = 1; i < isoline.cols(); i++) {
+      // Check that point lies on the G(y0)-isoline:
+      double err = std::abs(G(isoline.col(i)) - Gy0);
+      // Check that point is not stationary:
+      double step = (isoline.col(i) - isoline.col(i - 1)).norm();
+      if (step < tol * tol || err > tol) {
+        passed_computeIsolinePoints = false;
+        break;
+      }
+    }
+    if (passed_computeIsolinePoints)
+      std::cout << "Test for computeIsolinePoints() passed!\n\n";
+    else
+      std::cout << "Test for computeIsolinePoints() failed.\n\n";
+  } else
+    std::cout << "Test for computeIsolinePoints() failed: wrong size.\n\n";
+
+  /*
+   *  run ContourPlot::crookedEgg()
+   */
+  Eigen::MatrixXd crookedEggCurve = ContourPlot::crookedEgg();
+  std::cout << "Output of crookedEgg() is a " << crookedEggCurve.rows()
+            << " by " << crookedEggCurve.cols() << " matrix";
+  if (crookedEggCurve.cols() > 5)
+    std::cout << ", starting with\n" << crookedEggCurve.leftCols(5) << " ...\n";
+  else
+    std::cout << ":\n" << crookedEggCurve << std::endl;
+
+  /*
+   *  test ContourPlot::crookedEgg()
+   */
+  if (crookedEggCurve.rows() == 2 && crookedEggCurve.cols() >= 10) {
+    // ode45 takes at least 10 steps by default.
+    // Check initial state:
+    bool passed_crookedEgg = ((z0 - crookedEggCurve.col(0)).norm() < tol);
+    for (int i = 1; i < crookedEggCurve.cols(); i++) {
+      // Check that point lies on the 0-isoline:
+      double err = std::abs(F(crookedEggCurve.col(i)));
+      // Check that point is not stationary:
+      double step =
+          (crookedEggCurve.col(i) - crookedEggCurve.col(i - 1)).norm();
+      if (step < tol * tol || err > tol) {
+        passed_crookedEgg = false;
+        break;
+      }
+    }
+    if (passed_crookedEgg)
+      std::cout << "Test for crookedEgg() passed!\n\n";
+    else
+      std::cout << "Test for crookedEgg() failed.\n\n";
+  } else
+    std::cout << "Test for crookedEgg() failed: wrong size.\n\n";
+
+  /*
+   *  run ContourPlot::computeIsolinePointsDQ()
+   */
+  Eigen::MatrixXd isolineDQ = ContourPlot::computeIsolinePointsDQ(G, y0, 5.);
+  std::cout << "Output of computeIsolinePointsDQ() is a " << isolineDQ.rows()
+            << " by " << isolineDQ.cols() << " matrix";
+  if (isolineDQ.cols() > 5)
+    std::cout << ", starting with\n" << isolineDQ.leftCols(5) << " ...\n";
+  else
+    std::cout << ":\n" << isolineDQ << std::endl;
+
+  /*
+   *  test ContourPlot::computeIsolinePointsDQ()
+   */
+  if (isolineDQ.rows() == 2 && isolineDQ.cols() >= 10) {
+    // ode45 takes at least 10 steps by default.
+    // Check initial state:
+    bool passed_computeIsolinePointsDQ = ((y0 - isolineDQ.col(0)).norm() < tol);
+    double Gy0 = G(y0);
+    for (int i = 1; i < isolineDQ.cols(); i++) {
+      // Check that point lies on the G(y0)-isoline:
+      double err = std::abs(G(isolineDQ.col(i)) - Gy0);
+      // Check that point is not stationary:
+      double step = (isolineDQ.col(i) - isolineDQ.col(i - 1)).norm();
+      if (step < tol * tol || err > tol) {
+        passed_computeIsolinePointsDQ = false;
+        break;
+      }
+    }
+    if (passed_computeIsolinePointsDQ)
+      std::cout << "Test for computeIsolinePointsDQ() passed!\n\n";
+    else
+      std::cout << "Test for computeIsolinePointsDQ() failed.\n\n";
+  } else
+    std::cout << "Test for computeIsolinePointsDQ() failed: wrong size.\n\n";
+
+  return 0;
+}
