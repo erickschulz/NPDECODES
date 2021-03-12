03a5176dc209badd42bb0e29bf11501efe0a8ae5
Oliver Rietmann <rioliver@student.ethz.ch>
2021-03-12 16:51:10 +0100

    Integrated NLMatODE

---
+++ developers/NLMatODE/mastersolution/dependencies.cmake
@@ -0,0 +1,18 @@
+#if SOLUTION
+# Dependencies of mastersolution:
+#else
+# Add your custom dependencies here:
+#endif
+
+# DIR will be provided by the calling file.
+
+set(SOURCES
+  ${DIR}/nlmatode_main.cc
+  ${DIR}/nlmatode.h
+  ${DIR}/ode45.h
+  ${DIR}/polyfit.h
+)
+
+set(LIBRARIES
+  Eigen3::Eigen
+)
