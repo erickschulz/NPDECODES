5d06c58912d4919e991a5415f6351a1a2ff8a0a8
Oliver Rietmann <rioliver@student.ethz.ch>
2021-03-23 22:21:44 +0100

    Polish contourplot.h

--- developers/ContourPlot/mastersolution/contourplot.h
+++ developers/ContourPlot/mastersolution/contourplot.h
@@ -13,15 +13,14 @@ template <typename GradientFunctor>
 Eigen::Matrix<double, 2, Eigen::Dynamic> computeIsolinePoints(
     GradientFunctor &&gradF, Eigen::Vector2d y0, double T) {
   Eigen::Matrix<double, 2, Eigen::Dynamic> States;
-  // To do: (0-2.b)
-  // START
+
   // Right-hand-side vector field of isoline ODE
-  auto rhs = [gradF](const Eigen::Vector2d &x) {
+  auto rhs = [gradF](Eigen::Vector2d x) -> Eigen::Vector2d {
     Eigen::Vector2d gradFx = gradF(x);
-    Eigen::Vector2d out(-gradFx(1), gradFx(0));  // rotate
-    out /= gradFx.norm();                        // scale
-    return out;
+    return Eigen::Vector2d(-gradFx(1), gradFx(0)) / gradFx.norm();
   };
+
+#if SOLUTION
   // Adaptive explicit Runge-Kutta method
   ode45<Eigen::Vector2d> integrator(rhs);
   // Set tolerances for timestep control
@@ -31,33 +30,41 @@ Eigen::Matrix<double, 2, Eigen::Dynamic> computeIsolinePoints(
   std::vector<std::pair<Eigen::Vector2d, double>> sol = integrator.solve(y0, T);
   // Convert output into requested format: points on isoline arranged into the
   // columns of a matrix.
-  int N = sol.size() - 1;
-  States = Eigen::MatrixXd::Zero(2, N + 1);
-  for (int i = 0; i <= N; i++) {
-    States.col(i) = sol[i].first;
+  int M = sol.size() - 1;
+  States = Eigen::MatrixXd::Zero(2, M + 1);
+  for (int m = 0; m <= M; ++m) {
+    States.col(m) = sol[m].first;
   }
-  // END
+#else
+  //====================
+  // Your code goes here
+  // Use ode45 to solve the ODE with right-hand side
+  // given by the function rhs(...) and fill the
+  // matrix States with the solution vectors
+  //====================
+#endif
+
   return States;
 }
 /* SAM_LISTING_END_0 */
 
 /* SAM_LISTING_BEGIN_1 */
 Eigen::Matrix<double, 2, Eigen::Dynamic> crookedEgg() {
-  // crookedEggCurve will need to be reshaped to 2*(N+1).
-  Eigen::Matrix<double, 2, Eigen::Dynamic> crookedEggCurve;
-  // To do: (0-2.c)
-  // START
-  Eigen::Vector2d y0(1., 0.);
-  double T = 4.;
-  auto gradF = [](const Eigen::Vector2d &x) {
-    double x2 = x(0) * x(0), y2 = x(1) * x(1);
-    double x2y2 = 4. * (x2 + y2);
-    Eigen::Vector2d grad(x2y2 * x(0) - 3. * x2, x2y2 * x(1) - 3. * y2);
-    return grad;
+#if SOLUTION
+  auto gradF = [](Eigen::Vector2d x) -> Eigen::Vector2d {
+    return 4.0 * x.squaredNorm() * x - 3.0 * x.cwiseAbs2();
   };
-  crookedEggCurve = computeIsolinePoints(gradF, y0, T);
-  // END
-  return crookedEggCurve;
+  Eigen::Vector2d y0(1.0, 0.0);
+  double T = 4.0;
+  return computeIsolinePoints(gradF, y0, T);
+#else
+  //====================
+  // Your code goes here
+  // Replace the following dummy return value
+  // by the matrix containing the isoline points:
+  return Eigen::Matrix<double, 2, 42>::Zero();
+  //====================
+#endif
 }
 /* SAM_LISTING_END_1 */
 
@@ -65,42 +72,27 @@ Eigen::Matrix<double, 2, Eigen::Dynamic> crookedEgg() {
 template <typename FFunctor>
 Eigen::Matrix<double, 2, Eigen::Dynamic> computeIsolinePointsDQ(
     FFunctor &&F, Eigen::Vector2d y0, double T) {
-  // States will need to be reshaped to 2*(N+1).
-  Eigen::Matrix<double, 2, Eigen::Dynamic> States;
-  // To do: (0-2.d)
-  // START
+
+#if SOLUTION
   // Get gradient of F by symmetric difference quotients with
   // a span given by the root of the machine precision in order
   // to curb cancellation as much as possible.
-  double epsrt = std::sqrt(std::numeric_limits<double>::epsilon());
-  auto gradF = [epsrt, F](const Eigen::Vector2d &x) {
-    Eigen::Vector2d grad, dx, dy;
-    dx << epsrt, 0.;
-    dy << 0., epsrt;
-    grad(0) = (F(x + dx) - F(x - dx)) / (2 * epsrt);
-    grad(1) = (F(x + dy) - F(x - dy)) / (2 * epsrt);
-    return grad;
-  };
-  // The remainder as in computeIsolinePoints()
-  auto rhs = [gradF](const Eigen::Vector2d &x) {
-    Eigen::Vector2d gradFx = gradF(x);
-    Eigen::Vector2d out(-gradFx(1), gradFx(0));  // rotate
-    out /= gradFx.norm();                        // scale
-    return out;
+  double h = std::sqrt(std::numeric_limits<double>::epsilon());
+  auto gradF = [h, F](Eigen::Vector2d x) -> Eigen::Vector2d {
+    Eigen::Vector2d dx(h, 0.0);
+    Eigen::Vector2d dy(0.0, h);
+    return Eigen::Vector2d(F(x + dx) - F(x - dx), F(x + dy) - F(x - dy)) / (2.0 * h);
   };
 
-  ode45<Eigen::Vector2d> integrator(rhs);
-  integrator.options.rtol = 0.00001;
-  integrator.options.atol = 1e-9;
-
-  std::vector<std::pair<Eigen::Vector2d, double>> sol = integrator.solve(y0, T);
-  int N = sol.size() - 1;
-  States = Eigen::MatrixXd::Zero(2, N + 1);
-  for (int i = 0; i <= N; i++) {
-    States.col(i) = sol[i].first;
-  }
-  // END
-  return States;
+  return computeIsolinePoints(gradF, y0, T);
+#else
+  //====================
+  // Your code goes here
+  // Replace the following dummy return value
+  // by the matrix containing the isoline points:
+  return Eigen::Matrix<double, 2, 42>::Zero();
+  //====================
+#endif
 }
 /* SAM_LISTING_END_2 */
 
