1e4ae72f77c6ab83afd380af5ece1406bbe9aec6
Oliver Rietmann <rioliver@student.ethz.ch>
2021-04-01 16:25:48 +0200

    SDIRK: Added plot of stability domain

--- developers/SDIRK/mastersolution/sdirk_main.cc
+++ developers/SDIRK/mastersolution/sdirk_main.cc
@@ -1,9 +1,17 @@
+#include <cstdlib>
 #include <iostream>
 
 #include "sdirk.h"
 
+
 int main() {
+  // Compute convergence rates
   double rate = SDIRK::cvgSDIRK();
   std::cout << std::endl << "The rate is " << rate << std::endl;
+
+  // Plot stability domain for gamma = 1.0
+  std::system("python3 " CURRENT_SOURCE_DIR "/stabdomSDIRK.py " CURRENT_BINARY_DIR "/stabdomSDIRK.eps");
+  std::cout << "Generated " CURRENT_BINARY_DIR "/stabdomSDIRK.eps" << std::endl;
+
   return 0;
 }
