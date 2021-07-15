3a5c90735615e8669a6642d47e27d5f50f6049ec
Philippe Peter <phpeter@student.ethz.ch>
2021-07-15 09:34:05 +0200

    Adjusted plotting script

--- lecturecodes/ConvectionDiffusion/plot_layer.py
+++ lecturecodes/ConvectionDiffusion/plot_layer.py
@@ -4,22 +4,23 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 input_folder = os.path.abspath(sys.argv[1])
-output_folder = input_folder
+output_folder = os.path.join(input_folder, "plots")
+os.makedirs(output_folder, exist_ok=True)
 
 input_filenames = ["results_upwind.txt", "results_supg.txt", "results_standard_FEM.txt"]
-output_filenames = ["upwindcut.eps", "supgcut.eps", "standardfemcut.eps"]
+output_filenames = ["upwind.eps", "supg.eps", "standardfem.eps"]
 titles = ["Upwind Quadrature Solution", "Streamline-Diffusion (SUPG) Solution", "Standard FEM Solution"]
+
+
 for i in range(len(input_filenames)):
     file = os.path.join(input_folder,input_filenames[i])
     data = np.genfromtxt(file,delimiter=',')
     T = data[:,0]
-    values = data[:,1]
-    plt.plot(T,values)
+    values = data[:,3]
+    plt.plot(T,values, linewidth=0.8)
     plt.xlabel("$t$")
     plt.ylabel("$u(\gamma(t))$")
-    plt.suptitle(titles[i])
-    plt.title("along $\gamma(t) = (t,1-t)$")
-
-    plt.savefig(os.path.join(output_folder,output_filenames[i]))
+    plt.title(titles[i])
+    plt.savefig(os.path.join(output_folder,output_filenames[i]), dpi=200, bbox_inches="tight")
     plt.gcf().clear()
 
