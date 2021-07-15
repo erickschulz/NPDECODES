31ba112a3c77693870d4ae2b98ac53b0e56e29cb
Philippe Peter <phpeter@student.ethz.ch>
2021-07-15 10:14:09 +0200

    Polished main file for layer example

--- lecturecodes/ConvectionDiffusion/layer_main.cc
+++ lecturecodes/ConvectionDiffusion/layer_main.cc
@@ -1,89 +1,73 @@
 /**
- * @file upwindquadrature_main.cc
- * @brief NPDE homework template main
+ * @file layer_main.cc
+ * @brief Solves CD BVP for exact solution with an internal layer
  * @author Philippe Peter
- * @date June 2020
+ * @date July 2020
  * @copyright Developed at SAM, ETH Zurich
  */
-#include <lf/assemble/assemble.h>
-#include <lf/base/base.h>
 #include <lf/fe/fe.h>
 #include <lf/io/io.h>
 #include <lf/mesh/hybrid2d/hybrid2d.h>
-#include <lf/mesh/mesh.h>
-#include <lf/mesh/utils/utils.h>
 #include <lf/uscalfe/uscalfe.h>
 
 #include <Eigen/Core>
-#include <Eigen/SparseCore>
-#include <Eigen/SparseLU>
-#include <cmath>
 #include <memory>
+#include <string>
 
-#include "standard_galerkin.h"
-#include "convection_emp.h"
 #include "cd_tools.h"
-#include "solve_upwind.h"
-#include "streamline_upwind.h"
+#include "standard_fem.h"
+#include "supg.h"
+#include "upwind.h"
 
-int main(){
-    //parameter functions:
-    //boundary conditions
-    const auto g = [](const Eigen::Vector2d &x) {
-       return x(0) > x(1)? 1.0:0.0;
-    };
-    // velocity field
-    const auto v = [](const Eigen::Vector2d &x) {
-        return Eigen::Vector2d(1.0,1.0);
-    };
-    const auto eps = [](const Eigen::Vector2d &x) {
-       return std::pow(10,-10);
-    };
+int main() {
+  // parameter functions:
+  // boundary conditions
+  const auto g = [](const Eigen::Vector2d &x) {
+    return x(0) > x(1) ? 1.0 : 0.0;
+  };
+  // velocity field
+  const auto v = [](const Eigen::Vector2d &x) {
+    return Eigen::Vector2d(1.0, 1.0);
+  };
+  // diffusion coefficient
+  const auto eps = [](const Eigen::Vector2d &x) { return 10E-10; };
+  // source function
+  const auto f = [](const Eigen::Vector2d &x) { return 0.0; };
 
-    const auto f = [](const Eigen::Vector2d &x) {
-       return 0.0;
-    };
+  // construct mesh:
+  int M = 49 * 2;
 
-    //construct mesh:
-    int M = 49*2;
+  // Read Mesh from file
+  std::string mesh_file = CURRENT_SOURCE_DIR "/meshes/mesh_squre.msh";
+  auto mesh_factory = std::make_unique<lf::mesh::hybrid2d::MeshFactory>(2);
+  lf::io::GmshReader reader(std::move(mesh_factory), mesh_file);
+  auto mesh_p = reader.mesh();
 
-     // MESH CONSTRUCTION
-    // construct a triangular tensor product mesh on the unit square
-/*  
-    std::unique_ptr<lf::mesh::MeshFactory> mesh_factory_ptr =
-        std::make_unique<lf::mesh::hybrid2d::MeshFactory>(2);
-    lf::mesh::utils::TPTriagMeshBuilder builder(std::move(mesh_factory_ptr));
-    builder.setBottomLeftCorner(Eigen::Vector2d{0.0, 0.0})
-        .setTopRightCorner(Eigen::Vector2d{1.0, 1.0})
-        .setNumXCells(M)
-        .setNumYCells(M);
-    std::shared_ptr<lf::mesh::Mesh> mesh_p = builder.Build();
-*/
-    std::string mesh_file = "../../../lecturecodes/ConvectionDiffusion/mesh_square.gmsh";
-    auto mesh_factory = std::make_unique<lf::mesh::hybrid2d::MeshFactory>(2);
-    lf::io::GmshReader reader(std::move(mesh_factory),mesh_file);
-    auto mesh_p = reader.mesh();
+  // Construct dofhanlder for linear finite element space on the mesh.
+  auto fe_space =
+      std::make_shared<lf::uscalfe::FeSpaceLagrangeO1<double>>(mesh_p);
 
-    // DOF HANDLER & FINITE ELEMENT SPACE
-    // Construct dofhanlder for linear finite elements on the mesh.
-    auto fe_space =
-        std::make_shared<lf::uscalfe::FeSpaceLagrangeO1<double>>(mesh_p);
+  // Solve
+  Eigen::VectorXd sol_standard =
+      ConvectionDiffusion::SolveCDBVPStandardGalerkin(fe_space, eps, v, f, g);
+  lf::fe::MeshFunctionFE sol_standard_mf(fe_space, sol_standard);
 
-    Eigen::VectorXd sol_standard = ConvectionDiffusion::SolveCDBVPStandardGalerkin(fe_space,eps,v,f,g);
-    lf::fe::MeshFunctionFE sol_standard_mf(fe_space, sol_standard);
+  Eigen::VectorXd sol_stable =
+      ConvectionDiffusion::SolveCDBVPUpwind(fe_space, eps, v, f, g);
+  lf::fe::MeshFunctionFE sol_upwind_mf(fe_space, sol_stable);
 
-    Eigen::VectorXd sol_stable = ConvectionDiffusion::SolveCDBVPUpwind(fe_space,eps,v,f,g);
-    lf::fe::MeshFunctionFE sol_upwind_mf(fe_space, sol_stable);
+  Eigen::VectorXd sol_supg =
+      ConvectionDiffusion::SolveCDBVPSupg(fe_space, eps, v, f, g);
+  lf::fe::MeshFunctionFE sol_supg_mf(fe_space, sol_supg);
 
-    Eigen::VectorXd sol_supg = ConvectionDiffusion::SolveCDBVPSupg(fe_space,eps,v,f,g);
-    lf::fe::MeshFunctionFE sol_supg_mf(fe_space, sol_supg);
+  // Output solution along the curve gamma
+  auto gamma = [](double t) { return Eigen::Vector2d(t, 1 - t); };
+  ConvectionDiffusion::SampleMeshFunction("results_standard_FEM.txt", mesh_p,
+                                          gamma, sol_standard_mf, 1000);
+  ConvectionDiffusion::SampleMeshFunction("results_upwind.txt", mesh_p, gamma,
+                                          sol_upwind_mf, 1000);
+  ConvectionDiffusion::SampleMeshFunction("results_supg.txt", mesh_p, gamma,
+                                          sol_supg_mf, 1000);
 
-
-    auto gamma = [](double t){ return Eigen::Vector2d(t,1-t);};
-    ConvectionDiffusion::SampleMeshFunction("results_standard_FEM.txt", mesh_p, gamma, sol_standard_mf, 1000);
-    ConvectionDiffusion::SampleMeshFunction("results_upwind.txt", mesh_p, gamma, sol_upwind_mf, 1000);
-    ConvectionDiffusion::SampleMeshFunction("results_supg.txt", mesh_p, gamma, sol_supg_mf, 1000);
-
-    
-    return 0;
+  return 0;
 }
