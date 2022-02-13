1b37f89e2ed432f5bea0a6ce5fc05a6349fc4832
craffael <raffael@casagrande.ch>
2022-02-13 12:55:00 +0100

    use proper version of eigen

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -4,3 +4,5 @@ hunter_config(lehrfempp
   URL "https://github.com/craffael/lehrfempp/archive/c6e312d9fe68400543ed7b7cd8fa4ecf5d3ae729.tar.gz"
   SHA1 "92e91eacd4d52466d01aeb1ebfca1048af3709d3"
 )
+
+hunter_config(Eigen VERSION 3.3.9)
