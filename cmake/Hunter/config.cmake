2c5a9f47cbf09c11b0810b703e107f8b0536ec02
craffael <raffael@casagrande.ch>
2022-02-13 13:57:21 +0100

    use latest version of LehrFEM

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -1,8 +1,8 @@
 # This file specifies additional data for the dependencies that are imported via hunter
 
 hunter_config(lehrfempp
-  URL "https://github.com/craffael/lehrfempp/archive/df73fcd3860643f32e26e5985ad5b98f8e5a9057.tar.gz"
-  SHA1 "75795142fd048186d1a525601b28b644870fb1f3"
+  URL "https://github.com/craffael/lehrfempp/archive/9f73009add67095f656a67a456950edab41fb4d0.tar.gz"
+  SHA1 "a01729ad4a75ec880b6b0619228ddc16475a497c"
 )
 
 hunter_config(Eigen VERSION 3.3.9)
