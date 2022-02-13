5824dc7f36524f917e98445bed53ec69568336b2
craffael <raffael@casagrande.ch>
2022-02-13 20:16:41 +0100

    Fix CI (#30)
    * add boost::program_options
    
    * - Use latest version of LehrFEM++
    
    - Use Boost::program_options only if Lecturecodes are compiled
    - Fix Boost dependencies in lecturecodes
    
    * install matplotlib in CI environment
    
    * debug ci
    
    * debug ci
    
    * debug-ci
    
    * fix
    
    * use proper version of eigen
    
    * fix gcc-8 problem in DebuggingWithLLDB
    
    * use latest version of LehrFEM++
    
    * use latest version of LehrFEM
    
    * enable ci completly
    
    * debug osx
    
    * build cache
    
    * debug ci
    
    * debug ci
    
    * fix osx specific problems
    
    * enable all ci stages

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -1,6 +1,8 @@
 # This file specifies additional data for the dependencies that are imported via hunter
 
 hunter_config(lehrfempp
-  URL "https://github.com/craffael/lehrfempp/archive/de186a85b1824f669f6deb5033f3c9bdafb6abed.tar.gz"
-  SHA1 "843d3c1f9d2e0051043bf373d6d757fb1cb683a4"
+  URL "https://github.com/craffael/lehrfempp/archive/9f73009add67095f656a67a456950edab41fb4d0.tar.gz"
+  SHA1 "a01729ad4a75ec880b6b0619228ddc16475a497c"
 )
+
+hunter_config(Eigen VERSION 3.3.9)
